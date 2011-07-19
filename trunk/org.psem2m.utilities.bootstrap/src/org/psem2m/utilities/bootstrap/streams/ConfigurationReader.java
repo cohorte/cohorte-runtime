496e6a812aec348f438ddda8f2565a241cc33792
Thomas Calmant <thomas.calmant@isandlatech.com>
2011-07-19 18:22:17 +0200

    Ajout de la commande include:filename dans les fichiers d'entrée du bootstrap.

--- trunk/org.psem2m.utilities.bootstrap/src/org/psem2m/utilities/bootstrap/streams/ConfigurationReader.java
+++ trunk/org.psem2m.utilities.bootstrap/src/org/psem2m/utilities/bootstrap/streams/ConfigurationReader.java
@@ -5,6 +5,8 @@ package org.psem2m.utilities.bootstrap.streams;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -12,6 +14,7 @@ import java.io.ObjectInputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -23,6 +26,9 @@ import java.util.Set;
  */
 public class ConfigurationReader {
 
+    /** File include command */
+    public static final String INCLUDE_COMMAND = "include:";
+
     /**
      * Converts the given strings array into an URL array. Uses the file
      * protocol for malformed URLs.
@@ -33,35 +39,34 @@ public class ConfigurationReader {
      */
     public static URL[] stringsToURLs(final String[] aStringArray) {
 
-        List<URL> result = new ArrayList<URL>(aStringArray.length);
+	List<URL> result = new ArrayList<URL>(aStringArray.length);
 
-        for (String value : aStringArray) {
+	for (String value : aStringArray) {
+	    URL valueUrl = null;
+	    try {
+		// Try a direct conversion
+		valueUrl = new URL(value);
 
-            URL valueUrl = null;
-            try {
-                // Try a direct conversion
-                valueUrl = new URL(value);
+	    } catch (MalformedURLException e) {
+		// Try using the file protocol
+		File file = new File(value);
 
-            } catch (MalformedURLException e) {
-                // Try using the file protocol
-                File file = new File(value);
+		try {
+		    if (file.exists()) {
+			valueUrl = file.toURI().toURL();
+		    }
 
-                try {
-                    if (file.exists()) {
-                        valueUrl = file.toURI().toURL();
-                    }
+		} catch (MalformedURLException ex) {
+		    // Abandon this string
+		}
+	    }
 
-                } catch (MalformedURLException ex) {
-                    // Abandon this string
-                }
-            }
+	    if (valueUrl != null) {
+		result.add(valueUrl);
+	    }
+	}
 
-            if (valueUrl != null) {
-                result.add(valueUrl);
-            }
-        }
-
-        return result.toArray(new URL[0]);
+	return result.toArray(new URL[0]);
     }
 
     /** Configuration input stream */
@@ -74,7 +79,28 @@ public class ConfigurationReader {
      *            The configuration input stream
      */
     public ConfigurationReader(final InputStream aInputStream) {
-        pInputStream = aInputStream;
+	pInputStream = aInputStream;
+    }
+
+    /**
+     * Tries to open the file indicated in the include directive. Returns null
+     * if the file is not found.
+     * 
+     * @param aIncludeLine
+     *            Complete include directive line
+     * @return The file input stream, null on error
+     */
+    protected FileInputStream getIncludedFileStream(final String aIncludeLine) {
+
+	try {
+	    String fileName = aIncludeLine.substring(INCLUDE_COMMAND.length())
+		    .trim();
+
+	    return new FileInputStream(fileName);
+
+	} catch (FileNotFoundException e) {
+	    return null;
+	}
     }
 
     /**
@@ -85,67 +111,96 @@ public class ConfigurationReader {
      */
     public URL[] readSerializedConfiguration() {
 
-        Object readData = null;
+	Object readData = null;
 
-        try {
-            // Try to read some data
-            ObjectInputStream objectStream = new ObjectInputStream(pInputStream);
-            readData = objectStream.readObject();
-            objectStream.close();
+	try {
+	    // Try to read some data
+	    ObjectInputStream objectStream = new ObjectInputStream(pInputStream);
+	    readData = objectStream.readObject();
+	    objectStream.close();
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+	} catch (IOException e) {
+	    e.printStackTrace();
+	} catch (ClassNotFoundException e) {
+	    e.printStackTrace();
+	}
 
-        // Try to understand what we read (instanceof is false if null)
+	// Try to understand what we read (instanceof is false if null)
 
-        if (readData instanceof URL[]) {
-            // Direct array
-            return (URL[]) readData;
+	if (readData instanceof URL[]) {
+	    // Direct array
+	    return (URL[]) readData;
 
-        } else if (readData instanceof String[]) {
-            // String to URL conversion needed
-            return stringsToURLs((String[]) readData);
-        }
+	} else if (readData instanceof String[]) {
+	    // String to URL conversion needed
+	    return stringsToURLs((String[]) readData);
+	}
 
-        // Unknown format
-        return null;
+	// Unknown format
+	return null;
     }
 
     /**
-     * Read lines, removing duplications, from the standard input until the
-     * first empty line.
+     * Read lines, removing duplications, from the input given in the
+     * constructor until the first empty line.
      * 
      * @return The read lines content
      */
     public String[] readStringLines() {
+	return readStringLines(pInputStream);
+    }
 
-        // Use a set to avoid duplications
-        Set<String> linesSet = new HashSet<String>();
+    /**
+     * Read lines, removing duplications, from the given input until the first
+     * empty line.
+     * 
+     * @param aInputStream
+     *            The input stream to be used
+     * 
+     * @return The read lines content
+     */
+    protected String[] readStringLines(final InputStream aInputStream) {
 
-        // Read from standard input
-        final BufferedReader reader = new BufferedReader(new InputStreamReader(
-                pInputStream));
+	// Use a set to avoid duplications
+	Set<String> linesSet = new HashSet<String>();
 
-        // One bundle URL per line
-        try {
-            String readLine = reader.readLine();
+	// Read from standard input
+	final BufferedReader reader = new BufferedReader(new InputStreamReader(
+		aInputStream));
 
-            while (!readLine.isEmpty()) {
-                // Transform the line into an URL
-                linesSet.add(readLine);
+	// One bundle URL per line
+	try {
+	    String readLine = reader.readLine();
 
-                // Next step
-                readLine = reader.readLine();
-            }
+	    while (readLine != null && !readLine.isEmpty()) {
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		if (readLine.startsWith(INCLUDE_COMMAND)) {
+		    // File include
+		    InputStream includedStream = getIncludedFileStream(readLine);
 
-        return linesSet.toArray(new String[0]);
+		    if (includedStream != null) {
+			linesSet.addAll(Arrays
+				.asList(readStringLines(includedStream)));
+
+		    } else {
+			System.err.println("Can't open file : "
+				+ readLine.substring(INCLUDE_COMMAND.length()));
+		    }
+
+		} else {
+		    // Consider the line as an URL
+		    linesSet.add(readLine);
+		}
+
+		// Next step
+		readLine = reader.readLine();
+	    }
+
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+
+	return linesSet.toArray(new String[0]);
     }
 
     /**
@@ -155,6 +210,6 @@ public class ConfigurationReader {
      * @return An array of URLs.
      */
     public URL[] readURLLines() {
-        return stringsToURLs(readStringLines());
+	return stringsToURLs(readStringLines());
     }
 }
