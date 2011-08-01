bf57c8f74fc5aef31cc1bce21ddafe073c62a434
Thomas Calmant <thomas.calmant@isandlatech.com>
2011-08-01 16:14:01 +0200

    Recherche des bundles par leur nom symbolique dans le bootstrap
    * Quelques bugs de script à corriger
    * À copier dans le service BundleFinder

--- trunk/org.psem2m.utilities.bootstrap/src/org/psem2m/utilities/bootstrap/streams/ConfigurationReader.java
+++ trunk/org.psem2m.utilities.bootstrap/src/org/psem2m/utilities/bootstrap/streams/ConfigurationReader.java
@@ -29,226 +29,236 @@ import org.psem2m.utilities.bootstrap.IMessageSender;
  */
 public class ConfigurationReader {
 
-    /** Comment marker in configuration file */
-    public static final String COMMENT_MARKER = "#";
+	/** Comment marker in configuration file */
+	public static final String COMMENT_MARKER = "#";
 
-    /** File include command */
-    public static final String INCLUDE_COMMAND = "include:";
+	/** File include command */
+	public static final String INCLUDE_COMMAND = "include:";
 
-    /** Bundle finder */
-    private FileFinder pFileFinder;
+	/** Bundle finder */
+	private final FileFinder pFileFinder;
 
-    /** Configuration input stream */
-    private InputStream pInputStream;
+	/** Configuration input stream */
+	private final InputStream pInputStream;
 
-    /** Message sender (mainly for errors) */
-    private IMessageSender pMessageSender;
+	/** Message sender (mainly for errors) */
+	private final IMessageSender pMessageSender;
 
-    /**
-     * Prepares to read the given input stream
-     * 
-     * @param aInputStream
-     *            The configuration input stream
-     * @param aMessageSender
-     *            Log message sender
-     */
-    public ConfigurationReader(final InputStream aInputStream,
-	    final IMessageSender aMessageSender) {
+	/**
+	 * Prepares to read the given input stream
+	 * 
+	 * @param aInputStream
+	 *            The configuration input stream
+	 * @param aMessageSender
+	 *            Log message sender
+	 */
+	public ConfigurationReader(final InputStream aInputStream,
+			final IMessageSender aMessageSender) {
 
-	pInputStream = aInputStream;
-	pMessageSender = aMessageSender;
-	pFileFinder = new FileFinder(pMessageSender);
-    }
-
-    /**
-     * Tries to open the file indicated in the include directive. Returns null
-     * if the file is not found.
-     * 
-     * @param aIncludeLine
-     *            Complete include directive line
-     * @return The file input stream, null on error
-     */
-    protected FileInputStream getIncludedFileStream(final String aIncludeLine) {
-
-	try {
-	    String fileName = aIncludeLine.substring(INCLUDE_COMMAND.length())
-		    .trim();
-
-	    File file = pFileFinder.find(fileName);
-	    if (file != null) {
-		return new FileInputStream(file);
-	    }
-
-	    return null;
-
-	} catch (FileNotFoundException e) {
-	    return null;
-	}
-    }
-
-    /**
-     * Reads the first serialized object from the input stream. Return null if
-     * it wasn't an array of URL.
-     * 
-     * @return The deserialized URL array, null on error.
-     */
-    public URL[] readSerializedConfiguration() {
-
-	Object readData = null;
-
-	try {
-	    // Try to read some data
-	    ObjectInputStream objectStream = new ObjectInputStream(pInputStream);
-	    readData = objectStream.readObject();
-	    objectStream.close();
-
-	} catch (IOException e) {
-	    e.printStackTrace();
-	} catch (ClassNotFoundException e) {
-	    e.printStackTrace();
+		pInputStream = aInputStream;
+		pMessageSender = aMessageSender;
+		pFileFinder = new FileFinder();
 	}
 
-	// Try to understand what we read (instanceof is false if null)
+	/**
+	 * Tries to open the file indicated in the include directive. Returns null
+	 * if the file is not found.
+	 * 
+	 * @param aIncludeLine
+	 *            Complete include directive line
+	 * @return The file input stream, null on error
+	 */
+	protected FileInputStream getIncludedFileStream(final String aIncludeLine) {
 
-	if (readData instanceof URL[]) {
-	    // Direct array
-	    return (URL[]) readData;
+		try {
+			String fileName = aIncludeLine.substring(INCLUDE_COMMAND.length())
+					.trim();
 
-	} else if (readData instanceof String[]) {
-	    // String to URL conversion needed
-	    return stringsToURLs((String[]) readData);
+			File file = pFileFinder.find(fileName);
+			if (file != null) {
+				return new FileInputStream(file);
+			}
+
+			return null;
+
+		} catch (FileNotFoundException e) {
+			return null;
+		}
 	}
 
-	// Unknown format
-	return null;
-    }
+	/**
+	 * Reads the first serialized object from the input stream. Return null if
+	 * it wasn't an array of URL.
+	 * 
+	 * @return The deserialized URL array, null on error.
+	 */
+	public URL[] readSerializedConfiguration() {
 
-    /**
-     * Read lines, removing duplications, from the input given in the
-     * constructor until the first empty line.
-     * 
-     * @return The read lines content
-     */
-    public String[] readStringLines() {
-	return readStringLines(pInputStream);
-    }
+		Object readData = null;
 
-    /**
-     * Read lines, removing duplications, from the given input until the first
-     * empty line.
-     * 
-     * @param aInputStream
-     *            The input stream to be used
-     * 
-     * @return The read lines content
-     */
-    protected String[] readStringLines(final InputStream aInputStream) {
+		try {
+			// Try to read some data
+			ObjectInputStream objectStream = new ObjectInputStream(pInputStream);
+			readData = objectStream.readObject();
+			objectStream.close();
 
-	// Use a set to avoid duplications
-	Set<String> linesSet = new LinkedHashSet<String>();
-
-	// Read from standard input
-	final BufferedReader reader = new BufferedReader(new InputStreamReader(
-		aInputStream));
-
-	// One bundle URL per line
-	try {
-	    String readLine = reader.readLine();
-
-	    // readLine can be null if the end of file is reached
-	    while (readLine != null) {
-
-		// Trim the line
-		readLine = readLine.trim();
-
-		// Test the comment marker
-		if (readLine.isEmpty() || readLine.startsWith(COMMENT_MARKER)) {
-		    // Do nothing (ignore the other commands
-
-		} else if (readLine.startsWith(INCLUDE_COMMAND)) {
-		    // File include
-		    InputStream includedStream = getIncludedFileStream(readLine);
-
-		    if (includedStream != null) {
-			linesSet.addAll(Arrays
-				.asList(readStringLines(includedStream)));
-
-		    } else {
-
-			pMessageSender.sendMessage(
-				Level.WARNING,
-				"ConfigurationReader",
-				"readStringLines",
-				"Can't open file : "
-					+ readLine.substring(INCLUDE_COMMAND
-						.length()));
-		    }
-
-		} else {
-		    // Consider the line as an URL
-		    linesSet.add(readLine);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
 		}
 
-		// Next step
-		readLine = reader.readLine();
-	    }
+		// Try to understand what we read (instanceof is false if null)
 
-	} catch (IOException e) {
-	    e.printStackTrace();
-	}
+		if (readData instanceof URL[]) {
+			// Direct array
+			return (URL[]) readData;
 
-	return linesSet.toArray(new String[0]);
-    }
-
-    /**
-     * Utility method that calls {@link #readStringLines()} and converts its
-     * result into an array of URLs with {@link #stringsToURLs(String[])}.
-     * 
-     * @return An array of URLs.
-     */
-    public URL[] readURLLines() {
-	return stringsToURLs(readStringLines());
-    }
-
-    /**
-     * Converts the given strings array into an URL array. Uses the file
-     * protocol for malformed URLs.
-     * 
-     * @param aStringArray
-     *            Strings to be converted
-     * @return URL array corresponding to the strings
-     */
-    public URL[] stringsToURLs(final String[] aStringArray) {
-
-	List<URL> result = new ArrayList<URL>(aStringArray.length);
-
-	for (String value : aStringArray) {
-	    URL valueUrl = null;
-	    try {
-		// Try a direct conversion
-		valueUrl = new URL(value);
-
-	    } catch (MalformedURLException e) {
-		// Try using the file protocol
-		File file = pFileFinder.find(value);
-
-		if (file != null) {
-		    try {
-			// Accept the URL in any case at this time
-			// File existence will be tested later
-			valueUrl = file.toURI().toURL();
-
-		    } catch (MalformedURLException ex) {
-			// Abandon this string
-		    }
+		} else if (readData instanceof String[]) {
+			// String to URL conversion needed
+			return stringsToURLs((String[]) readData);
 		}
-	    }
 
-	    if (valueUrl != null) {
-		result.add(valueUrl);
-	    }
+		// Unknown format
+		return null;
 	}
 
-	return result.toArray(new URL[0]);
-    }
+	/**
+	 * Read lines, removing duplications, from the input given in the
+	 * constructor until the first empty line.
+	 * 
+	 * @return The read lines content
+	 */
+	public String[] readStringLines() {
+		return readStringLines(pInputStream);
+	}
+
+	/**
+	 * Read lines, removing duplications, from the given input until the first
+	 * empty line.
+	 * 
+	 * @param aInputStream
+	 *            The input stream to be used
+	 * 
+	 * @return The read lines content
+	 */
+	protected String[] readStringLines(final InputStream aInputStream) {
+
+		// Use a set to avoid duplications
+		Set<String> linesSet = new LinkedHashSet<String>();
+
+		// Read from standard input
+		final BufferedReader reader = new BufferedReader(new InputStreamReader(
+				aInputStream));
+
+		// One bundle URL per line
+		try {
+			String readLine = reader.readLine();
+
+			// readLine can be null if the end of file is reached
+			while (readLine != null) {
+
+				// Trim the line
+				readLine = readLine.trim();
+
+				// Test the comment marker
+				if (readLine.isEmpty() || readLine.startsWith(COMMENT_MARKER)) {
+					// Do nothing (ignore the other commands
+
+				} else if (readLine.startsWith(INCLUDE_COMMAND)) {
+					// File include
+					InputStream includedStream = getIncludedFileStream(readLine);
+
+					if (includedStream != null) {
+						linesSet.addAll(Arrays
+								.asList(readStringLines(includedStream)));
+
+					} else {
+
+						pMessageSender.sendMessage(
+								Level.WARNING,
+								"ConfigurationReader",
+								"readStringLines",
+								"Can't open file : "
+										+ readLine.substring(INCLUDE_COMMAND
+												.length()));
+					}
+
+				} else {
+					// Consider the line as an URL
+					linesSet.add(readLine);
+				}
+
+				// Next step
+				readLine = reader.readLine();
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return linesSet.toArray(new String[0]);
+	}
+
+	/**
+	 * Utility method that calls {@link #readStringLines()} and converts its
+	 * result into an array of URLs with {@link #stringsToURLs(String[])}.
+	 * 
+	 * @return An array of URLs.
+	 */
+	public URL[] readURLLines() {
+		return stringsToURLs(readStringLines());
+	}
+
+	/**
+	 * Converts the given strings array into an URL array. Uses the file
+	 * protocol for malformed URLs.
+	 * 
+	 * @param aStringArray
+	 *            Strings to be converted
+	 * @return URL array corresponding to the strings
+	 */
+	public URL[] stringsToURLs(final String[] aStringArray) {
+
+		List<URL> result = new ArrayList<URL>(aStringArray.length);
+
+		for (String value : aStringArray) {
+			URL valueUrl = null;
+			try {
+				// Try a direct conversion
+				valueUrl = new URL(value);
+
+			} catch (MalformedURLException e) {
+				// Try the file name
+				File file = pFileFinder.find(value);
+
+				if (file == null) {
+					// Try the symbolic name
+					file = pFileFinder.findBundle(value);
+				}
+
+				if (file != null) {
+					try {
+						// Accept the URL in any case at this time
+						// File existence will be tested later
+						valueUrl = file.toURI().toURL();
+
+					} catch (MalformedURLException ex) {
+						// Abandon this string
+					}
+
+				} else {
+					pMessageSender.sendMessage(Level.WARNING,
+							"Configuration Reader", "stringsToURLs",
+							"File not found : " + value);
+				}
+			}
+
+			if (valueUrl != null) {
+				result.add(valueUrl);
+			}
+		}
+
+		return result.toArray(new URL[0]);
+	}
 }
--- trunk/org.psem2m.utilities.bootstrap/src/org/psem2m/utilities/bootstrap/streams/FileFinder.java
+++ trunk/org.psem2m.utilities.bootstrap/src/org/psem2m/utilities/bootstrap/streams/FileFinder.java
@@ -6,16 +6,18 @@
 package org.psem2m.utilities.bootstrap.streams;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.logging.Level;
-
-import org.psem2m.utilities.bootstrap.IMessageSender;
+import java.util.Map;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
 
 /**
  * Implements the bundle finder, using the platform directories service
@@ -24,161 +26,267 @@ import org.psem2m.utilities.bootstrap.IMessageSender;
  */
 public class FileFinder {
 
-    /**
-     * contains the absolute path of the BASE folder of the current application
-     */
-    public final static String PROP_PLATFORM_BASE = "org.psem2m.platform.base";
+	/** Bundle symbolic name Manifest attribute name */
+	public static final String BUNDLE_SYMBOLIC_NAME = "Bundle-SymbolicName";
 
-    /**
-     * contains the absolute path of the HOME folder of the current application
-     */
-    public final static String PROP_PLATFORM_HOME = "org.psem2m.platform.home";
+	/**
+	 * contains the absolute path of the BASE folder of the current application
+	 */
+	public final static String PROP_PLATFORM_BASE = "org.psem2m.platform.base";
 
-    /** Platform base and home repository directory name */
-    public static final String REPOSITORY_NAME = "repo";
+	/**
+	 * contains the absolute path of the HOME folder of the current application
+	 */
+	public final static String PROP_PLATFORM_HOME = "org.psem2m.platform.home";
 
-    /** Message sender */
-    private IMessageSender pMessageSender;
+	/** Platform base and home repository directory name */
+	public static final String REPOSITORY_NAME = "repo";
 
-    /**
-     * Prepares the finder
-     * 
-     * @param aMessageSender
-     *            A message sender
-     */
-    public FileFinder(final IMessageSender aMessageSender) {
-	pMessageSender = aMessageSender;
-    }
+	/** Jar files list */
+	private final Map<String, File> pBundlesCache = new HashMap<String, File>();
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.psem2m.isolates.base.bundles.IBundleFinderSvc#findBundle(java.lang
-     * .String[])
-     */
-    public File find(final String... aFilePossibleNames) {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.psem2m.isolates.base.bundles.IBundleFinderSvc#findBundle(java.lang
+	 * .String[])
+	 */
+	public File find(final String... aFilePossibleNames) {
 
-	// Look in each repository
-	for (File repository : getRepositories()) {
+		// Look in each repository
+		for (File repository : getRepositories()) {
 
-	    if (!repository.exists() || !repository.isDirectory()) {
-		continue;
-	    }
+			if (!repository.exists() || !repository.isDirectory()) {
+				continue;
+			}
 
-	    // Look for each possible name
-	    for (String bundleName : aFilePossibleNames) {
+			// Look for each possible name
+			for (String bundleName : aFilePossibleNames) {
 
-		File bundleFile = new File(repository, bundleName);
-		if (bundleFile.exists()) {
-		    // Stop on first bundle found
-		    return bundleFile;
-		}
-	    }
-	}
-
-	// Bundle not found in repositories, tries the names as full ones
-	for (String fileName : aFilePossibleNames) {
-
-	    // Try 'local' file
-	    File bundleFile = new File(fileName);
-	    if (bundleFile.exists()) {
-		// Stop on first bundle found
-		return bundleFile;
-	    }
-
-	    // Try as a URI
-	    try {
-		URI bundleUri = new URI(fileName);
-		URL bundleUrl = bundleUri.toURL();
-
-		if (bundleUrl.getProtocol().equals("file")) {
-
-		    bundleFile = new File(bundleUri.getPath());
-		    if (bundleFile.exists()) {
-			return bundleFile;
-		    }
+				File bundleFile = new File(repository, bundleName);
+				if (bundleFile.exists()) {
+					// Stop on first bundle found
+					return bundleFile;
+				}
+			}
 		}
 
-	    } catch (MalformedURLException e) {
-		// Do nothing, we're determining the kind of element
-	    } catch (URISyntaxException e) {
-		// Do nothing, we're determining the kind of element
-	    } catch (IllegalArgumentException e) {
-		// Do nothing, the URI is not absolute
-	    }
+		// Bundle not found in repositories, tries the names as full ones
+		for (String fileName : aFilePossibleNames) {
+
+			// Try 'local' file
+			File bundleFile = new File(fileName);
+			if (bundleFile.exists()) {
+				// Stop on first bundle found
+				return bundleFile;
+			}
+
+			// Try as a URI
+			try {
+				URI bundleUri = new URI(fileName);
+				URL bundleUrl = bundleUri.toURL();
+
+				if (bundleUrl.getProtocol().equals("file")) {
+
+					bundleFile = new File(bundleUri.getPath());
+					if (bundleFile.exists()) {
+						return bundleFile;
+					}
+				}
+
+			} catch (MalformedURLException e) {
+				// Do nothing, we're determining the kind of element
+			} catch (URISyntaxException e) {
+				// Do nothing, we're determining the kind of element
+			} catch (IllegalArgumentException e) {
+				// Do nothing, the URI is not absolute
+			}
+		}
+
+		return null;
 	}
 
-	pMessageSender.sendMessage(Level.WARNING, "FileFinder", "find",
-		"File not found : " + Arrays.toString(aFilePossibleNames));
+	/**
+	 * Finds all bundles in the known repositories and stores their path and
+	 * symbolic names
+	 */
+	protected synchronized void findAllBundles() {
 
-	return null;
-    }
+		// Clear existing cache
+		pBundlesCache.clear();
 
-    /**
-     * <pre>
-     * org.psem2m.platform.base=[/Users/ogattaz/workspaces/psem2m/psem2m/platforms/felix.user.dir]
-     * </pre>
-     * 
-     * @return
-     */
-    public String getPlatformBase() {
-	return System.getProperty(PROP_PLATFORM_BASE);
-    }
-
-    /**
-     * <pre>
-     * org.psem2m.platform.home=[/usr/share/psem2m]
-     * </pre>
-     * 
-     * @return
-     */
-    public String getPlatformHome() {
-	return System.getProperty(PROP_PLATFORM_HOME);
-    }
-
-    public File[] getRepositories() {
-
-	List<File> repositories = new ArrayList<File>();
-
-	final String platformBase = getPlatformBase();
-	final String platformHome = getPlatformHome();
-
-	File baseRepo = null;
-	File homeRepo = null;
-
-	// Current instance repository
-	if (platformBase != null) {
-	    baseRepo = new File(getPlatformBase(), REPOSITORY_NAME);
-	    if (baseRepo.exists() && baseRepo.isDirectory()) {
-		repositories.add(baseRepo);
-	    }
+		// Do the job
+		for (File repository : getRepositories()) {
+			findAllBundles(repository);
+		}
 	}
 
-	// Home repository
-	if (platformHome != null) {
-	    homeRepo = new File(getPlatformHome(), REPOSITORY_NAME);
-	    if (!homeRepo.equals(baseRepo) && homeRepo.exists()
-		    && homeRepo.isDirectory()) {
-		repositories.add(homeRepo);
-	    }
+	/**
+	 * Completes the given list with all files having the extension ".jar"
+	 * 
+	 * @param aRootDirectory
+	 *            Root search directory
+	 */
+	protected void findAllBundles(final File aRootDirectory) {
+
+		File[] repoFiles = aRootDirectory.listFiles();
+		if (repoFiles != null) {
+
+			for (File file : repoFiles) {
+
+				if (file.isFile()
+						&& file.getName().toLowerCase().endsWith(".jar")) {
+					// Try to read it
+					String symbolicName = getBundleSymbolicName(file);
+					if (symbolicName != null) {
+						// Store it
+						pBundlesCache.put(symbolicName, file);
+					}
+
+				} else {
+					// Recursive search
+					findAllBundles(file);
+				}
+			}
+		}
 	}
 
-	if (platformBase != null) {
-	    baseRepo = new File(getPlatformBase());
-	    if (baseRepo.exists() && baseRepo.isDirectory()) {
-		repositories.add(baseRepo);
-	    }
+	/**
+	 * Tries to find the first bundle with the given symbolic name
+	 * 
+	 * @param aSymbolicName
+	 *            A bundle symbolic name
+	 * @return The first corresponding file found, null if none found
+	 */
+	public File findBundle(final String aSymbolicName) {
+
+		if (aSymbolicName == null) {
+			return null;
+		}
+
+		// Be sure we have some files to look into
+		if (pBundlesCache.isEmpty()) {
+			findAllBundles();
+		}
+
+		return pBundlesCache.get(aSymbolicName);
 	}
 
-	if (platformHome != null) {
-	    homeRepo = new File(getPlatformHome());
-	    if (!homeRepo.equals(baseRepo) && homeRepo.exists()
-		    && homeRepo.isDirectory()) {
-		repositories.add(homeRepo);
-	    }
+	/**
+	 * Retrieves the bundle symbolic name from the manifest of the given file,
+	 * null if unreadable
+	 * 
+	 * @param file
+	 *            A bundle Jar file
+	 * @return The bundle symbolic name, null if unreadable
+	 */
+	public String getBundleSymbolicName(final File file) {
+
+		if (!file.exists() || !file.isFile()) {
+			// Ignore invalid files
+			return null;
+		}
+
+		try {
+			JarFile jarFile = new JarFile(file);
+			Manifest jarManifest = jarFile.getManifest();
+
+			if (jarManifest == null) {
+				// Ignore files without Manifest
+				return null;
+			}
+
+			Attributes attributes = jarManifest.getMainAttributes();
+			if (attributes != null) {
+
+				// Handle symbolic name format
+				String symbolicName = attributes.getValue(BUNDLE_SYMBOLIC_NAME);
+				if (symbolicName != null) {
+
+					// Test if there is an extra information (version,
+					// singleton, ...)
+					int endOfName = symbolicName.indexOf(';');
+					if (endOfName == -1) {
+						return symbolicName;
+					}
+
+					// Only return the symbolic name part
+					return symbolicName.substring(0, endOfName);
+				}
+			}
+
+		} catch (IOException ex) {
+			// Ignore
+		}
+
+		return null;
 	}
 
-	return repositories.toArray(new File[0]);
-    }
+	/**
+	 * <pre>
+	 * org.psem2m.platform.base=[/Users/ogattaz/workspaces/psem2m/psem2m/platforms/felix.user.dir]
+	 * </pre>
+	 * 
+	 * @return
+	 */
+	public String getPlatformBase() {
+		return System.getProperty(PROP_PLATFORM_BASE);
+	}
+
+	/**
+	 * <pre>
+	 * org.psem2m.platform.home=[/usr/share/psem2m]
+	 * </pre>
+	 * 
+	 * @return
+	 */
+	public String getPlatformHome() {
+		return System.getProperty(PROP_PLATFORM_HOME);
+	}
+
+	public File[] getRepositories() {
+
+		List<File> repositories = new ArrayList<File>();
+
+		final String platformBase = getPlatformBase();
+		final String platformHome = getPlatformHome();
+
+		File baseRepo = null;
+		File homeRepo = null;
+
+		// Current instance repository
+		if (platformBase != null) {
+			baseRepo = new File(getPlatformBase(), REPOSITORY_NAME);
+			if (baseRepo.exists() && baseRepo.isDirectory()) {
+				repositories.add(baseRepo);
+			}
+		}
+
+		// Home repository
+		if (platformHome != null) {
+			homeRepo = new File(getPlatformHome(), REPOSITORY_NAME);
+			if (!homeRepo.equals(baseRepo) && homeRepo.exists()
+					&& homeRepo.isDirectory()) {
+				repositories.add(homeRepo);
+			}
+		}
+
+		if (platformBase != null) {
+			baseRepo = new File(getPlatformBase());
+			if (baseRepo.exists() && baseRepo.isDirectory()) {
+				repositories.add(baseRepo);
+			}
+		}
+
+		if (platformHome != null) {
+			homeRepo = new File(getPlatformHome());
+			if (!homeRepo.equals(baseRepo) && homeRepo.exists()
+					&& homeRepo.isDirectory()) {
+				repositories.add(homeRepo);
+			}
+		}
+
+		return repositories.toArray(new File[0]);
+	}
 }
