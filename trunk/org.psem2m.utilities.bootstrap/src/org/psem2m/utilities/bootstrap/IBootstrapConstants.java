ab5c8610e9a71df1cb8685862889a306b37c297d
Thomas Calmant <thomas.calmant@isandlatech.com>
2011-08-01 18:18:30 +0200

    Le bootstrap envoie des IsolateStatus en plus des LogRecords

--- trunk/org.psem2m.utilities.bootstrap/src/org/psem2m/utilities/bootstrap/IBootstrapConstants.java
+++ trunk/org.psem2m.utilities.bootstrap/src/org/psem2m/utilities/bootstrap/IBootstrapConstants.java
@@ -12,32 +12,32 @@ package org.psem2m.utilities.bootstrap;
  */
 public interface IBootstrapConstants {
 
-    /** Bootstrap properties prefix */
-    String BOOTSTRAP_PROPERTY_PREFIX = IBootstrapConstants.PLATFORM_PROPERTY_PREFIX
-	    + ".bootstrap.";
+	/** Bootstrap properties prefix */
+	String BOOTSTRAP_PROPERTY_PREFIX = IBootstrapConstants.PLATFORM_PROPERTY_PREFIX
+			+ ".bootstrap.";
 
-    /** Framework configuration key */
-    String CONFIG_FRAMEWORK = IBootstrapConstants.BOOTSTRAP_PROPERTY_PREFIX
-	    + "osgi.framework";
+	/** Framework configuration key */
+	String CONFIG_FRAMEWORK = IBootstrapConstants.BOOTSTRAP_PROPERTY_PREFIX
+			+ "osgi.framework";
 
-    /** Help command */
-    String HELP_COMMAND = "help";
+	/** Help command */
+	String HELP_COMMAND = "help";
 
-    /** Output human readable lines, not serialized ones */
-    String HUMAN_OUTPUT_FLAG = "human";
+	/** Output human readable lines, not serialized ones */
+	String HUMAN_OUTPUT_FLAG = "human";
 
-    /** Platform property prefix */
-    String PLATFORM_PROPERTY_PREFIX = "org.psem2m";
+	/** Platform property prefix */
+	String PLATFORM_PROPERTY_PREFIX = "org.psem2m";
 
-    /** Read lines from the given file */
-    String READ_FROM_FILE = "file";
+	/** Read lines from the given file */
+	String READ_FROM_FILE = "file";
 
-    /** Read lines from input */
-    String READ_LINES_COMMAND = "lines";
+	/** Read lines from input */
+	String READ_LINES_COMMAND = "lines";
 
-    /** Test command (writes a serialized URL array in a file) */
-    String TEST_COMMAND = "test";
+	/** Test command (writes a serialized URL array in a file) */
+	String TEST_COMMAND = "test";
 
-    /** Read array object from input */
-    String UNSERIALIZE_COMMAND = "serialized";
+	/** Read array object from input */
+	String UNSERIALIZE_COMMAND = "serialized";
 }
--- trunk/org.psem2m.utilities.bootstrap/src/org/psem2m/utilities/bootstrap/streams/MessageSender.java
+++ trunk/org.psem2m.utilities.bootstrap/src/org/psem2m/utilities/bootstrap/streams/MessageSender.java
@@ -10,7 +10,9 @@ import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
+import org.psem2m.isolates.base.boot.IsolateStatus;
 import org.psem2m.utilities.bootstrap.IMessageSender;
+import org.psem2m.utilities.bootstrap.Main;
 
 /**
  * Writes log records on the given object output stream.
@@ -19,161 +21,188 @@ import org.psem2m.utilities.bootstrap.IMessageSender;
  */
 public class MessageSender implements IMessageSender {
 
-    /** Logger name */
-    public static final String LOGGER_NAME = "Bootstrap.MessageSender";
+	/** Logger name */
+	public static final String LOGGER_NAME = "Bootstrap.MessageSender";
 
-    /** Message in human readable mode */
-    private boolean pHumanMode;
+	/** Message in human readable mode */
+	private boolean pHumanMode;
 
-    /** The object output stream */
-    private OutputStream pOutputStream;
+	/** The object output stream */
+	private final OutputStream pOutputStream;
 
-    /**
-     * Prepares the message sender
-     * 
-     * @param aOutputStream
-     *            The output stream
-     * @param aHumanMode
-     *            Set human readable output mode
-     */
-    public MessageSender(final OutputStream aOutputStream) {
-	pOutputStream = aOutputStream;
-    }
-
-    /**
-     * Appends the throwable stack trace to given string builder
-     * 
-     * @param aBuilder
-     *            A string builder
-     * @param aThrowable
-     *            A thrown error
-     */
-    protected void appendThrowable(final StringBuilder aBuilder,
-	    final Throwable aThrowable) {
-
-	if (aBuilder == null || aThrowable == null) {
-	    return;
+	/**
+	 * Prepares the message sender
+	 * 
+	 * @param aOutputStream
+	 *            The output stream
+	 * @param aHumanMode
+	 *            Set human readable output mode
+	 */
+	public MessageSender(final OutputStream aOutputStream) {
+		pOutputStream = aOutputStream;
 	}
 
-	aBuilder.append(aThrowable);
-	aBuilder.append(" :: ");
-	aBuilder.append(aThrowable.getMessage());
-	aBuilder.append("\n");
-	aThrowable.printStackTrace();
+	/**
+	 * Appends the throwable stack trace to given string builder
+	 * 
+	 * @param aBuilder
+	 *            A string builder
+	 * @param aThrowable
+	 *            A thrown error
+	 */
+	protected void appendThrowable(final StringBuilder aBuilder,
+			final Throwable aThrowable) {
 
-	StackTraceElement[] stackTrace = aThrowable.getStackTrace();
-	for (StackTraceElement element : stackTrace) {
-	    aBuilder.append("\tat ");
-	    aBuilder.append(element);
-	    aBuilder.append("\n");
+		if (aBuilder == null || aThrowable == null) {
+			return;
+		}
+
+		aBuilder.append(aThrowable);
+		aBuilder.append(" :: ");
+		aBuilder.append(aThrowable.getMessage());
+		aBuilder.append("\n");
+		aThrowable.printStackTrace();
+
+		StackTraceElement[] stackTrace = aThrowable.getStackTrace();
+		for (StackTraceElement element : stackTrace) {
+			aBuilder.append("\tat ");
+			aBuilder.append(element);
+			aBuilder.append("\n");
+		}
+
+		Throwable cause = aThrowable.getCause();
+		if (cause != null) {
+			appendThrowable(aBuilder, cause);
+		}
 	}
 
-	Throwable cause = aThrowable.getCause();
-	if (cause != null) {
-	    appendThrowable(aBuilder, cause);
-	}
-    }
+	/**
+	 * Small log record formatter for human output
+	 * 
+	 * @param aLogRecord
+	 *            Log record to format
+	 * @return A human readable version of the log record
+	 */
+	protected String formatRecord(final LogRecord aLogRecord) {
 
-    /**
-     * Small log record formatter for human output
-     * 
-     * @param aLogRecord
-     *            Log record to format
-     * @return A human readable version of the log record
-     */
-    protected String formatRecord(final LogRecord aLogRecord) {
+		StringBuilder builder = new StringBuilder();
+		builder.append("[");
+		builder.append(aLogRecord.getLoggerName());
+		builder.append("][");
+		builder.append(aLogRecord.getLevel());
+		builder.append("] ");
+		builder.append(aLogRecord.getSourceClassName());
+		builder.append(".");
+		builder.append(aLogRecord.getSourceMethodName());
+		builder.append(" :: ");
+		builder.append(aLogRecord.getMessage());
 
-	StringBuilder builder = new StringBuilder();
-	builder.append("[");
-	builder.append(aLogRecord.getLoggerName());
-	builder.append("][");
-	builder.append(aLogRecord.getLevel());
-	builder.append("] ");
-	builder.append(aLogRecord.getSourceClassName());
-	builder.append(".");
-	builder.append(aLogRecord.getSourceMethodName());
-	builder.append(" :: ");
-	builder.append(aLogRecord.getMessage());
+		appendThrowable(builder, aLogRecord.getThrown());
 
-	appendThrowable(builder, aLogRecord.getThrown());
-
-	builder.append("\n");
-	return builder.toString();
-    }
-
-    /**
-     * Sends the given message via a LogRecord object serialized on the standard
-     * input
-     * 
-     * @param aLevel
-     *            Message level
-     * @param aSourceClass
-     *            Source class name
-     * @param aSourceMethod
-     *            Source method name
-     * @param aMessage
-     *            Log message
-     */
-    @Override
-    public void sendMessage(final Level aLevel,
-	    final CharSequence aSourceClass, final CharSequence aSourceMethod,
-	    final CharSequence aMessage) {
-
-	sendMessage(aLevel, aSourceClass, aSourceMethod, aMessage, null);
-    }
-
-    /**
-     * Sends the given message via a LogRecord object serialized on the standard
-     * input
-     * 
-     * @param aLevel
-     *            Message level
-     * @param aSourceClass
-     *            Source class name
-     * @param aSourceMethod
-     *            Source method name
-     * @param aMessage
-     *            Log message
-     * @param aThrowable
-     *            An exception
-     */
-    @Override
-    public void sendMessage(final Level aLevel,
-	    final CharSequence aSourceClass, final CharSequence aSourceMethod,
-	    final CharSequence aMessage, final Throwable aThrowable) {
-
-	String message = aMessage.toString();
-	if (message.trim().isEmpty()) {
-	    // Ignore empty messages
-	    return;
+		builder.append("\n");
+		return builder.toString();
 	}
 
-	LogRecord record = new LogRecord(aLevel, message);
-	record.setLoggerName("Bootstrap");
-	record.setSourceClassName(aSourceClass.toString());
-	record.setSourceMethodName(aSourceMethod.toString());
-	record.setThrown(aThrowable);
+	/**
+	 * Sends the given message via a LogRecord object serialized on the standard
+	 * input
+	 * 
+	 * @param aLevel
+	 *            Message level
+	 * @param aSourceClass
+	 *            Source class name
+	 * @param aSourceMethod
+	 *            Source method name
+	 * @param aMessage
+	 *            Log message
+	 */
+	@Override
+	public void sendMessage(final Level aLevel,
+			final CharSequence aSourceClass, final CharSequence aSourceMethod,
+			final CharSequence aMessage) {
 
-	try {
-	    if (pHumanMode) {
-		pOutputStream.write(formatRecord(record).getBytes());
-
-	    } else {
-		((ObjectOutputStream) pOutputStream).writeObject(record);
-	    }
-
-	} catch (IOException e) {
-	    Logger.getLogger(LOGGER_NAME).log(record);
+		sendMessage(aLevel, aSourceClass, aSourceMethod, aMessage, null);
 	}
-    }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.psem2m.utilities.bootstrap.IMessageSender#setHumanMode(boolean)
-     */
-    @Override
-    public void setHumanMode(final boolean aHumanMode) {
-	pHumanMode = aHumanMode;
-    }
+	/**
+	 * Sends the given message via a LogRecord object serialized on the standard
+	 * input
+	 * 
+	 * @param aLevel
+	 *            Message level
+	 * @param aSourceClass
+	 *            Source class name
+	 * @param aSourceMethod
+	 *            Source method name
+	 * @param aMessage
+	 *            Log message
+	 * @param aThrowable
+	 *            An exception
+	 */
+	@Override
+	public void sendMessage(final Level aLevel,
+			final CharSequence aSourceClass, final CharSequence aSourceMethod,
+			final CharSequence aMessage, final Throwable aThrowable) {
+
+		String message = aMessage.toString();
+		if (message.trim().isEmpty()) {
+			// Ignore empty messages
+			return;
+		}
+
+		LogRecord record = new LogRecord(aLevel, message);
+		record.setLoggerName("Bootstrap");
+		record.setSourceClassName(aSourceClass.toString());
+		record.setSourceMethodName(aSourceMethod.toString());
+		record.setThrown(aThrowable);
+
+		try {
+			if (pHumanMode) {
+				pOutputStream.write(formatRecord(record).getBytes());
+
+			} else {
+				((ObjectOutputStream) pOutputStream).writeObject(record);
+			}
+
+		} catch (IOException e) {
+			Logger.getLogger(LOGGER_NAME).log(record);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.psem2m.utilities.bootstrap.IMessageSender#sendStatus(int,
+	 * double)
+	 */
+	@Override
+	public void sendStatus(final int aState, final double aProgress) {
+
+		IsolateStatus status = new IsolateStatus(Main.getIsolateId(), aState,
+				aProgress);
+
+		try {
+			if (pHumanMode) {
+				sendMessage(Level.INFO, LOGGER_NAME, "sendStatus",
+						status.toString());
+
+			} else {
+				((ObjectOutputStream) pOutputStream).writeObject(status);
+			}
+
+		} catch (IOException ex) {
+			Logger.getLogger(LOGGER_NAME).log(Level.SEVERE,
+					"Error sending isolate status", ex);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.psem2m.utilities.bootstrap.IMessageSender#setHumanMode(boolean)
+	 */
+	@Override
+	public void setHumanMode(final boolean aHumanMode) {
+		pHumanMode = aHumanMode;
+	}
 }
