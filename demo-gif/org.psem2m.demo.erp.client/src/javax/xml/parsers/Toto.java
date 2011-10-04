53cfb450394fcecd1f30f1c4b77fded11103d970
Thomas Calmant <thomas.calmant@isandlatech.com>
2011-10-04 15:09:02 +0200

    Export factice des bundles XML
    Export de javax.xml.transform, javax.xml.parsers, org.w3c.dom et org.xml.sax

---
+++ demo-gif/org.psem2m.demo.erp.client/src/javax/xml/parsers/Toto.java
@@ -0,0 +1,13 @@
+/**
+ * File:   Toto.java
+ * Author: "Thomas Calmant"
+ * Date:   4 oct. 2011
+ */
+package javax.xml.parsers;
+
+/**
+ * Fake class to export the package
+ */
+public class Toto {
+
+}
