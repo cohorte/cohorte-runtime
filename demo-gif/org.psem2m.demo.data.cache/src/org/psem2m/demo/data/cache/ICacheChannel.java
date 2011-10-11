07b3f6903b4e31d2f7304d889cb6cc9559366ed9
Thomas Calmant <thomas.calmant@isandlatech.com>
2011-10-11 17:52:52 +0200

    Ajout d'un nouveau modèle de cache

---
+++ demo-gif/org.psem2m.demo.data.cache/src/org/psem2m/demo/data/cache/ICacheChannel.java
@@ -0,0 +1,60 @@
+/**
+ * File:   ICacheChannel.java
+ * Author: Thomas Calmant
+ * Date:   11 oct. 2011
+ */
+package org.psem2m.demo.data.cache;
+
+/**
+ * Describes a cache channel
+ * 
+ * @author Thomas Calmant
+ */
+public interface ICacheChannel<K, V> {
+
+    /**
+     * Closes the channel
+     */
+    void close();
+
+    /**
+     * Retrieves the cached object associated to the given key
+     * 
+     * @param aKey
+     *            A reference key
+     * @return The associated object, null if none
+     */
+    CachedObject<V> get(K aKey);
+
+    /**
+     * Retrieves a random object from the channel, null if the channel is empty
+     * 
+     * @return A random object, or null
+     */
+    CachedObject<V> getRandomObject();
+
+    /**
+     * Tests if the channel is empty
+     * 
+     * @return True if the channel is empty
+     */
+    boolean isEmpty();
+
+    /**
+     * Puts or replace the given value at the given key
+     * 
+     * @param aKey
+     *            A reference key
+     * @param aValue
+     *            The cached value
+     * @return The previous cached value, null if none
+     */
+    CachedObject<V> put(K aKey, V aValue);
+
+    /**
+     * Retrieves the number of cached objects
+     * 
+     * @return the number of cached objects
+     */
+    long size();
+}
---
+++ demo-gif/org.psem2m.demo.data.cache/src/org/psem2m/demo/data/cache/impl/CacheChannelImpl.java
@@ -0,0 +1,121 @@
+/**
+ * File:   CacheChannelImpl.java
+ * Author: Thomas Calmant
+ * Date:   11 oct. 2011
+ */
+package org.psem2m.demo.data.cache.impl;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.psem2m.demo.data.cache.CachedObject;
+import org.psem2m.demo.data.cache.ICacheChannel;
+
+/**
+ * Implementation of a cache channel
+ * 
+ * @author Thomas Calmant
+ */
+public class CacheChannelImpl<K, V> implements ICacheChannel<K, V> {
+
+    /** The internal map */
+    private final Map<K, CachedObject<V>> pCacheMap = new HashMap<K, CachedObject<V>>();
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.psem2m.demo.data.cache.ICacheChannel#close()
+     */
+    @Override
+    public void close() {
+
+        pCacheMap.clear();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.psem2m.demo.data.cache.ICacheChannel#get(java.lang.Object)
+     */
+    @Override
+    public CachedObject<V> get(final K aKey) {
+
+        return pCacheMap.get(aKey);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.psem2m.demo.data.cache.ICacheChannel#getRandomObject()
+     */
+    @Override
+    public CachedObject<V> getRandomObject() {
+
+        final int nbObjects = pCacheMap.size();
+        if (nbObjects == 0) {
+            // No objects, no random
+            return null;
+        }
+
+        final int randomIndex = (int) Math.random() * nbObjects;
+
+        // Get the ID at the selected index
+        K randomObjectKey = null;
+        final Iterator<K> iterator = pCacheMap.keySet().iterator();
+        int i = 0;
+        while (iterator.hasNext()) {
+
+            if (i == randomIndex) {
+                // Found !
+                randomObjectKey = iterator.next();
+                break;
+            }
+
+            // Continue
+            i++;
+            iterator.next();
+        }
+
+        return pCacheMap.get(randomObjectKey);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.psem2m.demo.data.cache.ICacheChannel#isEmpty()
+     */
+    @Override
+    public boolean isEmpty() {
+
+        return pCacheMap.isEmpty();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.psem2m.demo.data.cache.ICacheChannel#put(java.lang.Object,
+     * java.lang.Object)
+     */
+    @Override
+    public CachedObject<V> put(final K aKey, final V aValue) {
+
+        if (aValue == null) {
+            return pCacheMap.remove(aKey);
+
+        } else {
+            return pCacheMap.put(aKey, new CachedObject<V>(aValue));
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.psem2m.demo.data.cache.ICacheChannel#size()
+     */
+    @Override
+    public long size() {
+
+        return pCacheMap.size();
+    }
+}
