7b6dfdf008ad614699662af5d7c16e36b5b1a267
Thomas Calmant <thomas.calmant@isandlatech.com>
2011-10-13 19:12:39 +0200

    Correction de Jabsorb pour le multi-threading
    sendAndReceived devient synchronized.

--- remote-services/jabsorb.modified/src/org/jabsorb/client/HTTPSession.java
+++ remote-services/jabsorb.modified/src/org/jabsorb/client/HTTPSession.java
@@ -42,131 +42,123 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Transport session straightforwardly implemented in HTTP. As compared to the built-in 
- * URLConnectionSession, it allows more control over HTTP transport parameters, for
- * example, proxies and the support for HTTPS.
+ * Transport session straightforwardly implemented in HTTP. As compared to the
+ * built-in URLConnectionSession, it allows more control over HTTP transport
+ * parameters, for example, proxies and the support for HTTPS.
  * 
- * <p>To use this transport you need to first register it in the TransportRegistry, for example: <p>
- * <code>
- * 		HTTPSession.register(TransportRegistry.i());
- * </code>
+ * <p>To use this transport you need to first register it in the
+ * TransportRegistry, for example: <p> <code>
+ * HTTPSession.register(TransportRegistry.i()); </code>
  */
-public class HTTPSession implements Session
-{
-  private final static Logger log = LoggerFactory.getLogger(HTTPSession.class);
+public class HTTPSession implements Session {
+    static class Factory implements SessionFactory {
+        public Session newSession(final URI uri) {
 
-  protected HttpClient  client;
-
-  protected HttpState   state;
-
-  protected URI         uri;
-
-  public HTTPSession(URI uri)
-  {
-    this.uri = uri;
-  }
-
-  /**
-   * An option to set state from the outside. for example, to provide existing
-   * session parameters.
-   */
-  public void setState(HttpState state)
-  {
-    this.state = state;
-  }
-
-  /**
-   * As per JSON-RPC Working Draft
-   * http://json-rpc.org/wd/JSON-RPC-1-1-WD-20060807.html#RequestHeaders
-   */
-  static final String JSON_CONTENT_TYPE = "application/json";
-
-  public JSONObject sendAndReceive(JSONObject message)
-  {
-    try
-    {
-      if (log.isDebugEnabled())
-      {
-        log.debug("Sending: " + message.toString(2));
-      }
-      PostMethod postMethod = new PostMethod(uri.toString());
-      postMethod.setRequestHeader("Content-Type", "text/plain");
-
-      RequestEntity requestEntity = new StringRequestEntity(message.toString(),
-          JSON_CONTENT_TYPE, null);
-      postMethod.setRequestEntity(requestEntity);
-//      http().getHostConfiguration().setProxy(proxyHost, proxyPort);
-      http().executeMethod(null, postMethod, state);
-      int statusCode = postMethod.getStatusCode();
-      if (statusCode != HttpStatus.SC_OK)
-        throw new ClientError("HTTP Status - "
-            + HttpStatus.getStatusText(statusCode) + " (" + statusCode + ")");
-      JSONTokener tokener = new JSONTokener(postMethod
-          .getResponseBodyAsString());
-      Object rawResponseMessage = tokener.nextValue();
-      JSONObject responseMessage = (JSONObject) rawResponseMessage;
-      if (responseMessage == null)
-        throw new ClientError("Invalid response type - "
-            + rawResponseMessage.getClass());
-      return responseMessage;
+            return new HTTPSession(uri);
+        }
     }
-    catch (HttpException e)
-    {
-      throw new ClientError(e);
-    }
-    catch (IOException e)
-    {
-      throw new ClientError(e);
-    }
-    catch (JSONException e)
-    {
-      throw new ClientError(e);
-    }
-  }
-  
-  /**
-   * Expose commons-httpclient host configuration, for
-   * setting configuration parameters like proxy.
-   * 
-   * @return host configuration of the current HttpClient object 
-   */
-  public HostConfiguration getHostConfiguration() {
-	  return http().getHostConfiguration();
-  }
 
-  HttpClient http()
-  {
-    if (client == null)
-    {
-      client = new HttpClient();
-      if (state == null)
-      {
-        state = new HttpState();
-      }
-      client.setState(state);
+    /**
+     * As per JSON-RPC Working Draft
+     * http://json-rpc.org/wd/JSON-RPC-1-1-WD-20060807.html#RequestHeaders
+     */
+    static final String JSON_CONTENT_TYPE = "application/json";
+
+    private final static Logger log = LoggerFactory
+            .getLogger(HTTPSession.class);
+
+    /**
+     * Register this transport in 'registry'
+     */
+    public static void register(final TransportRegistry registry) {
+
+        registry.registerTransport("http", new Factory());
     }
-    return client;
-  }
 
-  public void close()
-  {
-    state.clear();
-    state = null;
-  }
+    protected HttpClient client;
 
-  static class Factory implements SessionFactory
-  {
-    public Session newSession(URI uri)
-    {
-      return new HTTPSession(uri);
+    protected HttpState state;
+
+    protected URI uri;
+
+    public HTTPSession(final URI uri) {
+
+        this.uri = uri;
     }
-  }
 
-  /**
-   * Register this transport in 'registry'
-   */
-  public static void register(TransportRegistry registry)
-  {
-    registry.registerTransport("http", new Factory());
-  }
+    public void close() {
+
+        state.clear();
+        state = null;
+    }
+
+    /**
+     * Expose commons-httpclient host configuration, for setting configuration
+     * parameters like proxy.
+     * 
+     * @return host configuration of the current HttpClient object
+     */
+    public HostConfiguration getHostConfiguration() {
+
+        return http().getHostConfiguration();
+    }
+
+    HttpClient http() {
+
+        if (client == null) {
+            client = new HttpClient();
+            if (state == null) {
+                state = new HttpState();
+            }
+            client.setState(state);
+        }
+        return client;
+    }
+
+    public synchronized JSONObject sendAndReceive(final JSONObject message) {
+
+        try {
+            if (log.isDebugEnabled()) {
+                log.debug("Sending: " + message.toString(2));
+            }
+            PostMethod postMethod = new PostMethod(uri.toString());
+            postMethod.setRequestHeader("Content-Type", "text/plain");
+
+            RequestEntity requestEntity = new StringRequestEntity(
+                    message.toString(), JSON_CONTENT_TYPE, null);
+            postMethod.setRequestEntity(requestEntity);
+            // http().getHostConfiguration().setProxy(proxyHost, proxyPort);
+            http().executeMethod(null, postMethod, state);
+            int statusCode = postMethod.getStatusCode();
+            if (statusCode != HttpStatus.SC_OK) {
+                throw new ClientError("HTTP Status - "
+                        + HttpStatus.getStatusText(statusCode) + " ("
+                        + statusCode + ")");
+            }
+            JSONTokener tokener = new JSONTokener(
+                    postMethod.getResponseBodyAsString());
+            Object rawResponseMessage = tokener.nextValue();
+            JSONObject responseMessage = (JSONObject) rawResponseMessage;
+            if (responseMessage == null) {
+                throw new ClientError("Invalid response type - "
+                        + rawResponseMessage.getClass());
+            }
+            return responseMessage;
+        } catch (HttpException e) {
+            throw new ClientError(e);
+        } catch (IOException e) {
+            throw new ClientError(e);
+        } catch (JSONException e) {
+            throw new ClientError(e);
+        }
+    }
+
+    /**
+     * An option to set state from the outside. for example, to provide existing
+     * session parameters.
+     */
+    public void setState(final HttpState state) {
+
+        this.state = state;
+    }
 }
