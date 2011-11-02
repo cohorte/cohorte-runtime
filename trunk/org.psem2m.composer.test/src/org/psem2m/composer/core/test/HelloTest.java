7014bce8a144fa0160bb005f159c97ed01221bc7
Thomas Calmant <thomas.calmant@isandlatech.com>
2011-11-02 11:55:56 +0100

    Composer Hello Test fournit un service

--- trunk/org.psem2m.composer.test/src/org/psem2m/composer/core/test/HelloTest.java
+++ trunk/org.psem2m.composer.test/src/org/psem2m/composer/core/test/HelloTest.java
@@ -1,28 +1,33 @@
 /**
  * File:   HelloTest.java
- * Author: "Thomas Calmant"
+ * Author: Thomas Calmant
  * Date:   27 oct. 2011
  */
 package org.psem2m.composer.core.test;
 
 import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Provides;
 import org.apache.felix.ipojo.annotations.Requires;
 import org.osgi.framework.BundleException;
+import org.psem2m.composer.test.api.IHelloSvc;
 import org.psem2m.isolates.base.IIsolateLoggerSvc;
 import org.psem2m.isolates.base.activators.CPojoBase;
 
 /**
- * @author "Thomas Calmant"
+ * Simple "Hello World" service for tests
  * 
+ * @author Thomas Calmant
  */
 @Component(name = "hello-test")
-public class HelloTest extends CPojoBase {
+@Provides(specifications = IHelloSvc.class)
+public class HelloTest extends CPojoBase implements IHelloSvc {
 
+    /** The logger */
     @Requires(id = "logger")
     private IIsolateLoggerSvc pLogger;
 
     /**
-     * 
+     * Default constructor
      */
     public HelloTest() {
 
@@ -37,8 +42,29 @@ public class HelloTest extends CPojoBase {
     @Override
     public void invalidatePojo() throws BundleException {
 
-        System.out.println("Hello test stopped");
-        pLogger.logInfo(this, "INVALIDATE", "HELLO TEST GONE");
+        pLogger.logInfo(this, "invalidatePojo", "Composer.Test.Hello Gone");
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.psem2m.composer.core.test.IHelloSvc#logHello(java.lang.String)
+     */
+    @Override
+    public void logHello(final String aName) {
+
+        pLogger.logInfo(this, "logHello", sayHello(aName));
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.psem2m.composer.core.test.IHelloSvc#sayHello(java.lang.String)
+     */
+    @Override
+    public String sayHello(final String aName) {
+
+        return "Hello, " + aName + " !";
     }
 
     /*
@@ -49,7 +75,6 @@ public class HelloTest extends CPojoBase {
     @Override
     public void validatePojo() throws BundleException {
 
-        System.out.println("Hello test started");
-        pLogger.logInfo(this, "VALIDATE", "HELLO TEST READY");
+        pLogger.logInfo(this, "validatePojo", "Composer.Test.Hello Ready");
     }
 }
