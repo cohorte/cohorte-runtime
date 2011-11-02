8b60de43f68321f93b147ae13e2ed3293c17532e
Thomas Calmant <thomas.calmant@isandlatech.com>
2011-11-02 12:04:49 +0100

    Ajout d'un consommateur de HelloService

--- trunk/org.psem2m.composer.test/src/org/psem2m/composer/core/test/HelloConsumerTest.java
+++ trunk/org.psem2m.composer.test/src/org/psem2m/composer/core/test/HelloConsumerTest.java
@@ -7,7 +7,6 @@ package org.psem2m.composer.core.test;
 
 import org.apache.felix.ipojo.annotations.Component;
 import org.apache.felix.ipojo.annotations.Invalidate;
-import org.apache.felix.ipojo.annotations.Provides;
 import org.apache.felix.ipojo.annotations.Requires;
 import org.apache.felix.ipojo.annotations.Validate;
 import org.osgi.framework.BundleException;
@@ -20,18 +19,21 @@ import org.psem2m.isolates.base.activators.CPojoBase;
  * 
  * @author Thomas Calmant
  */
-@Component(name = "hello-test")
-@Provides(specifications = IHelloSvc.class)
-public class HelloTest extends CPojoBase implements IHelloSvc {
+@Component(name = "hello-consumer-test")
+public class HelloConsumerTest extends CPojoBase {
 
     /** The logger */
     @Requires(id = "logger")
     private IIsolateLoggerSvc pLogger;
 
+    /** The test service */
+    @Requires(id = "hello-svc")
+    private IHelloSvc pService;
+
     /**
      * Default constructor
      */
-    public HelloTest() {
+    public HelloConsumerTest() {
 
         super();
     }
@@ -51,28 +53,6 @@ public class HelloTest extends CPojoBase implements IHelloSvc {
     /*
      * (non-Javadoc)
      * 
-     * @see org.psem2m.composer.core.test.IHelloSvc#logHello(java.lang.String)
-     */
-    @Override
-    public void logHello(final String aName) {
-
-        pLogger.logInfo(this, "logHello", sayHello(aName));
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.psem2m.composer.core.test.IHelloSvc#sayHello(java.lang.String)
-     */
-    @Override
-    public String sayHello(final String aName) {
-
-        return "Hello, " + aName + " !";
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
      * @see org.psem2m.isolates.base.activators.CPojoBase#validatePojo()
      */
     @Override
@@ -80,5 +60,10 @@ public class HelloTest extends CPojoBase implements IHelloSvc {
     public void validatePojo() throws BundleException {
 
         pLogger.logInfo(this, "validatePojo", "Composer.Test.Hello Ready");
+
+        pLogger.logInfo(this, "validatePojo", "Say hello = ",
+                pService.sayHello("ConsumerTester"));
+
+        pService.logHello("from ConsumerTester");
     }
 }
