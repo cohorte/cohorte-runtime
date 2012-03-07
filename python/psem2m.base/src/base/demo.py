d19d8daf44c855602c24105b065bacca4385d4c1
Thomas Calmant <thomas.calmant@isandlatech.com>
2012-03-07 16:57:05 +0100

    Démo devient une implémentation du Fallback

--- python/psem2m.base/src/base/demo.py
+++ python/psem2m.base/src/base/demo.py
@@ -8,46 +8,81 @@ Created on 29 févr. 2012
 # ------------------------------------------------------------------------------
 
 import logging
+from psem2m.component import constants
 _logger = logging.getLogger(__name__)
 
 # ------------------------------------------------------------------------------
 
 from psem2m.component.decorators import ComponentFactory, Provides, \
-    Validate, Invalidate, Property
+    Validate, Invalidate, Property, Requires
 
 # ------------------------------------------------------------------------------
 
-@ComponentFactory("erp-proxy-json-rpc")
-@Provides("org.psem2m.composer.demo.IComponent")
+SPEC_ICOMPONENT = "org.psem2m.composer.demo.IComponent"
+
+@ComponentFactory("fall-back")
+@Provides(SPEC_ICOMPONENT)
+@Property("_name", constants.IPOPO_INSTANCE_NAME)
 @Property("__export", "service.exported.interfaces", "*")
-class ErpProxy(object):
+@Requires("second", SPEC_ICOMPONENT)
+@Requires("next", SPEC_ICOMPONENT, optional=True)
+class FallBack(object):
 
     def __init__(self):
         """
         Constructor
         """
-        pass
+        self.second = None
+        self.next = None
+        self.logger = None
+        self._name = None
 
 
     def computeResult(self, component_context):
         """
         Implementation of org.psem2m.composer.demo.IComponent
+        
+        :param component_context: A ComponentContext object
+        :return: A ComponentContext object
         """
-        # TODO: implement it
-        return None
+        self.logger.warning("computeResult :\n%s", component_context)
+
+        result = component_context
+        use_fallback = True
+
+        if self.next is not None:
+            self.logger.info("First choice seems present")
+
+            try:
+                result = self.next.computeResult(component_context)
+                use_fallback = result is None or \
+                                (not result["results"] and result["errors"])
+
+            except:
+                self.logger.exception("Error calling the primary component")
+
+        if use_fallback:
+            # Reset context if needed
+            if result["errors"]:
+                result.errors = []
+
+            if result["results"]:
+                result["results"] = []
+
+            self.logger.info("Using fall-back")
+            result = self.second.computeResult(component_context)
+
+        self.logger.debug("Fallback result : %s", result)
+        return result
 
 
-    def test(self):
+    def getName(self):
         """
-        Dummy method
+        Retrieve the component name
+        
+        :return: The component name
         """
-        return "Hello, World !"
-
-    def hello(self, name):
-        """
-        Dummy method
-        """
-        return "Hello, %s !" % name
+        return self._name
 
 
     @Validate
@@ -55,7 +90,10 @@ class ErpProxy(object):
         """
         Component validated
         """
-        _logger.warn("Python ERP Proxy ON")
+        self.logger = logging.getLogger(self._name)
+        self.logger.info("Python FallBack Proxy ON")
+        self.logger.debug("> next = %s", self.next)
+        self.logger.debug("> fall = %s", self.second)
 
 
     @Invalidate
@@ -63,4 +101,5 @@ class ErpProxy(object):
         """
         Component invalidated
         """
-        _logger.warn("Python ERP Proxy OFF")
+        self.logger.info("Python FallBack Proxy OFF")
+        self.logger = None
