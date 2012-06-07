4b36e51c82f9df30f7ca731f0ec895625c3eeaad
Thomas Calmant <thomas.calmant@isandlatech.com>
2012-06-07 17:56:02 +0200

    Mise à jour de make_javadoc.sh
    - Prend en compte tous les dossiers src/ de /trunk/java/

--- make_javadoc.sh
+++ make_javadoc.sh
@@ -3,29 +3,30 @@
 # iPOJO Annotations library
 if [ -z $IPOJO_ANNOTATIONS ]
 then
-	IPOJO_ANNOTATIONS="platforms/org.apache.felix.ipojo.annotations-1.9.0-SNAPSHOT.jar"
+    IPOJO_ANNOTATIONS="platforms/org.apache.felix.ipojo.annotations-1.9.0-SNAPSHOT.jar"
 fi
 
 # Root projects containers (space separated folders)
 if [ -z $ROOT_FOLDERS ]
 then
-	ROOT_FOLDERS="trunk/ remote-services/"
+    ROOT_FOLDERS="trunk/java/"
 fi
 
 # Class path
 if [ -z $LIB_PATH ]
 then
-	LIB_PATH="$IPOJO_ANNOTATIONS:platforms/felix/*"
+    LIB_PATH="$IPOJO_ANNOTATIONS:platforms/felix/*"
 fi
 
 # Javadoc output folder
 if [ -z $OUTPUT_FOLDER ]
 then
-	OUTPUT_FOLDER="javadoc/"
+    OUTPUT_FOLDER="javadoc/"
 fi
 
 # Maxdepth 2 : _root_/_project_/src
-SRC_PATH=`find $ROOT_FOLDERS -maxdepth 2 -type d -name src | tr "\\n" ":"`
+# SRC_PATH=`find $ROOT_FOLDERS -maxdepth 2 -type d -name src | tr "\\n" ":"`
+SRC_PATH=`find $ROOT_FOLDERS -type d -name src | tr "\\n" ":"`
 
 # Remove previous run information
 rm -fr "$OUTPUT_FOLDER"
@@ -33,8 +34,8 @@ mkdir -p "$OUTPUT_FOLDER"
 
 # Do the job...
 javadoc -d "$OUTPUT_FOLDER" \
-	-sourcepath "$SRC_PATH" \
-	-classpath "$LIB_PATH" \
-	-subpackages org.psem2m \
-	2>"$OUTPUT_FOLDER/log.txt"
+    -sourcepath "$SRC_PATH" \
+    -classpath "$LIB_PATH" \
+    -subpackages org.psem2m \
+    2>"$OUTPUT_FOLDER/log.txt"
 
