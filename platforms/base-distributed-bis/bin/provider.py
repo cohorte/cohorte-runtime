dd3561f39d9dc7de615e7dfcff5165235dc3fc07
Thomas Calmant <thomas.calmant@isandlatech.com>
2012-06-08 12:04:31 +0200

    Ajout d'un fichier manquant
    Le "provider" côté demo.central, dans le dossier bin/ (ignoré par git)

---
+++ platforms/base-distributed-bis/bin/provider.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+#-- Content-Encoding: UTF-8 --
+"""
+Created on 29 févr. 2012
+
+:author: Thomas Calmant
+"""
+
+# ------------------------------------------------------------------------------
+
+import logging
+import threading
+import time
+
+_logger = logging.getLogger(__name__)
+
+# ------------------------------------------------------------------------------
+
+from pelix.ipopo import constants
+
+from pelix.ipopo.decorators import ComponentFactory, Provides, \
+    Validate, Invalidate, Property, Requires
+
+# ------------------------------------------------------------------------------
+
+SPEC_PROVIDER = "org.psem2m.dist.demo.provider"
+
+@ComponentFactory("demo.provider")
+@Provides(SPEC_PROVIDER)
+@Property("_name", constants.IPOPO_INSTANCE_NAME)
+@Property("__export", "service.exported.interfaces", "*")
+class DemoProvider(object):
+    """
+    Demo provider
+    """
+    def __init__(self):
+        """
+        Constructor
+        """
+        self._name = None
+        self._count = 0
+
+
+    def echo(self, name):
+        """
+        Says hello locally and returns the hello sentence
+        """
+        self._count += 1
+        sentence = "Hello from %s, %s ! You are the visitor %03d" \
+                    % (self._name, name, self._count)
+
+        _logger.info(sentence)
+        return sentence
+
+    @Validate
+    def validate(self, context):
+        """
+        Validation
+        """
+        self._count = 0
+        _logger.info("Component %s validated", self._name)
+
+
+    @Invalidate
+    def invalidate(self, context):
+        """
+        Invalidation
+        """
+        _logger.info("Component %s invalidated after %d visits", self._name,
+                     self._count)
