3486e87f8567b87e2fe82bc8e8903295513b89ff
Thomas Calmant <thomas.calmant@isandlatech.com>
2012-06-18 15:17:45 +0200

    PyHeartbeat: compatible avec ForkersAggregator
    - Basé sur une socket UDP multicast
    - Envoie un paquet toutes les 3 secondes

--- trunk/python/psem2m.forker/src/psem2m/forker/heartbeat.py
+++ trunk/python/psem2m.forker/src/psem2m/forker/heartbeat.py
@@ -21,21 +21,216 @@ from pelix.ipopo.decorators import ComponentFactory, Instantiate, \
 import logging
 _logger = logging.getLogger(__name__)
 
+import socket
+import struct
+import sys
 import threading
 
 # ------------------------------------------------------------------------------
 
-# The heartbeat signal
-SIGNAL_HEARTBEAT = "/psem2m/internals/forkers/heart-beat"
+# Heart beat packet type
+PACKET_TYPE_HEARTBEAT = 1
+
+# ------------------------------------------------------------------------------
+
+if sys.version_info[0] == 3:
+    # Python 3
+    def _to_bytes(data, encoding="UTF-8"):
+        """
+        Converts the given string to a bytes array
+        """
+        if type(data) is bytes:
+            # Nothing to do
+            return data
+
+        return data.encode(encoding)
+
+else:
+    # Python 2
+    def _to_bytes(data, encoding="UTF-8"):
+        """
+        Converts the given string to a bytes array
+        """
+        if type(data) is str:
+            # Nothing to do
+            return data
+
+        return data.encode(encoding)
+
+# ------------------------------------------------------------------------------
+
+def make_mreq(family, address):
+    """
+    Makes a mreq structure object for the given address and socket family.
+    
+    :param family: A socket family (AF_INET or AF_INET6)
+    :param address: A multicast address
+    :raise ValueError: Invalid family or address
+    """
+    if not address:
+        raise ValueError("Empty address")
+
+    # Convert the address to a binary form
+    group_bin = socket.inet_pton(family, address)
+
+    if family == socket.AF_INET:
+        # IPv4
+        # struct ip_mreq
+        # {
+        #     struct in_addr imr_multiaddr; /* IP multicast address of group */
+        #     struct in_addr imr_interface; /* local IP address of interface */
+        # };
+        # "=I" : Native order, standard size unsigned int
+        return group_bin + struct.pack("=I", socket.INADDR_ANY)
+
+    elif family == socket.AF_INET6:
+        # IPv6
+        # struct ipv6_mreq {
+        #    struct in6_addr ipv6mr_multiaddr;
+        #    unsigned int    ipv6mr_interface;
+        # };
+        # "@I" : Native order, native size unsigned int
+        return group_bin + struct.pack("@I", 0)
+
+    raise ValueError("Unknown family %s", family)
+
+
+def create_multicast_socket(address, port):
+    """
+    Creates a multicast socket according to the given address and port.
+    Handles both IPv4 and IPv6 addresses.
+    
+    :raise ValueError: Invalid address or port
+    """
+    # Get the information about a datagram (UDP) socket, of any family
+    try:
+        addr_info = socket.getaddrinfo(address, port, socket.AF_UNSPEC,
+                                       socket.SOCK_DGRAM)
+    except socket.gaierror:
+        raise ValueError("Error retrieving address informations (%s, %s)" \
+                         % (address, port))
+
+    if len(addr_info) > 1:
+        _logger.debug("More than one address information found. "
+                      "Using the first one.")
+
+    # Get the first entry : (family, socktype, proto, canonname, sockaddr)
+    addr_info = addr_info[0]
+
+    # Only accept IPv4/v6 addresses
+    if addr_info[0] not in (socket.AF_INET, socket.AF_INET6):
+        # Unhandled address family
+        raise ValueError("Unhandled socket family : %d" % (addr_info[0]))
+
+    # Prepare the socket
+    sock = socket.socket(addr_info[0], socket.SOCK_DGRAM)
+
+    # Reuse address
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    if hasattr(socket, 'SO_REUSEPORT'):
+        # Special for MacOS
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+
+    # Bind the socket
+    if sock.family == socket.AF_INET:
+        # IPv4 binding
+        sock.bind(('0.0.0.0', port))
+
+    else:
+        # IPv6 Binding
+        sock.bind(('::', port))
+
+    # Prepare the mreq structure to join the group
+    # addrinfo[4] = (addr,port)
+    mreq = make_mreq(sock.family, addr_info[4][0])
+
+    # Join the group
+    if sock.family == socket.AF_INET:
+        # IPv4
+        sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+        # Allow multicast packets to get back on this host
+        sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 1)
+
+    else:
+        # IPv6
+        sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, mreq)
+
+        # Allow multicast packets to get back on this host
+        sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_LOOP, 1)
+
+    return (sock, addr_info[4][0])
+
+
+def close_multicast_socket(sock, address):
+    """
+    Cleans up the given multicast socket.
+    Unregisters it of the multicast group.
+    
+    Parameters should be the result of create_multicast_socket
+    
+    :param sock: A multicast socket
+    :param address: The multicast address used by the socket
+    """
+    if not sock:
+        return
+
+    if address:
+        # Prepare the mreq structure to join the group
+        mreq = make_mreq(sock.family, address)
+
+        # Quit group
+        if sock.family == socket.AF_INET:
+            # IPv4
+            sock.setsockopt(socket.IPPROTO_IP, socket.IP_DROP_MEMBERSHIP, mreq)
+
+        elif sock.family == socket.AF_INET6:
+            # IPv6
+            sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_LEAVE_GROUP, mreq)
+
+    # Close the socket
+    sock.close()
+
+# ------------------------------------------------------------------------------
+
+def make_heartbeat(port, isolate_id, node_id):
+    """
+    Prepares the heart beat UDP packet
+    
+    Format : Little endian
+    * Packet type (1 byte)
+    * Signals port (2 bytes)
+    * Isolate ID length (2 bytes)
+    * Isolate ID (variable, UTF-8)
+    * Node ID length (2 bytes)
+    * Node ID (variable, UTF-8)
+    
+    :param port: The Signals access port
+    :param isolate_id: The ID of this isolate
+    :param node_id: The host node ID
+    :return: The heart beat packet content (byte array)
+    """
+    # Type and port...
+    packet = struct.pack("<BH", PACKET_TYPE_HEARTBEAT, port)
+
+    for string in (isolate_id, node_id):
+        # Strings...
+        string_bytes = _to_bytes(string)
+        packet += struct.pack("<H", len(string_bytes))
+        packet += string_bytes
+
+    return packet
 
 # ------------------------------------------------------------------------------
 
 @ComponentFactory("psem2m-forker-heart-factory")
 @Instantiate("psem2m-forker-heart")
+@Requires("_config", "org.psem2m.isolates.services.conf.ISvcConfig")
 # Just to have the same life cycle than the forker...
 @Requires("_forker", "org.psem2m.isolates.services.forker.IForker")
 # To retrieve access information
 @Requires("_http", "HttpService")
+@Requires("_receiver", "org.psem2m.signals.ISignalReceiver")
 @Requires("_sender", "org.psem2m.signals.ISignalBroadcaster")
 class Heart(object):
     """
@@ -45,11 +240,20 @@ class Heart(object):
         """
         Constructor
         """
+        self._config = None
         self._forker = None
         self._http = None
+        self._receiver = None
         self._sender = None
 
+        # Bundle context
         self._context = None
+
+        # Socket
+        self._socket = None
+        self._target = None
+
+        # Thread control
         self._event = None
         self._thread = None
 
@@ -58,32 +262,17 @@ class Heart(object):
         """
         Heart beat sender
         """
-        # Prepare the access information according to the HTTP service
-        server_info = {"node": self._context.get_property('psem2m.isolate.node'),
-                       "port": self._http.get_port()
-                       }
+        # Prepare the packet
+        beat = make_heartbeat(self._http.get_port(),
+                              self._context.get_property('psem2m.isolate.id'),
+                              self._context.get_property('psem2m.isolate.node'))
 
         while not self._event.is_set():
-            # Send the heart beat
-            self._sender.send(SIGNAL_HEARTBEAT, server_info,
-                              groups=["MONITORS"])
+            # Send the heart beat using the multicast socket
+            self._socket.sendto(beat, 0, self._target)
 
             # Wait 3 seconds before next loop
             self._event.wait(3)
-
-
-    @Validate
-    def validate(self, context):
-        """
-        Component validated
-        """
-        self._context = context
-
-        # Prepare the thread controls
-        self._event = threading.Event()
-
-        self._thread = threading.Thread(target=self._run, name="HeartBeart")
-        self._thread.start()
 
 
     @Invalidate
@@ -94,10 +283,46 @@ class Heart(object):
         # Get out of the waiting condition
         self._event.set()
 
+        # Unregister to the signals
+        # self._receiver.unregister_listener(SIGNAL_MATCH_ALL, self)
+
         # Wait for the thread to stop
-        self._thread.join(1)
+        self._thread.join(.5)
+
+        # Close the socket
+        close_multicast_socket(self._socket, self._target[0])
 
         # Clean up
         self._context = None
         self._event = None
+        self._socket = None
+        self._target = None
         self._thread = None
+
+
+    @Validate
+    def validate(self, context):
+        """
+        Component validated
+        """
+        self._context = context
+
+        # Get the multicast configuration
+        group = self._config.get_application().get_multicast_group()
+        port = self._config.get_application().get_multicast_port()
+
+        # Create the socket
+        self._socket, address = create_multicast_socket(group, port)
+
+        # Store group information
+        self._target = (address, port)
+
+        # Prepare the thread controls
+        self._event = threading.Event()
+
+        # Register to signals
+        # self._receiver.register_listener(SIGNAL_MATCH_ALL, self)
+
+        # Start the heart
+        self._thread = threading.Thread(target=self._run, name="HeartBeart")
+        self._thread.start()
