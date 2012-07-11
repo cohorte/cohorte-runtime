03d3f53ddc986886a7eaaa0eb544b45c19db5cc4
Thomas Calmant <thomas.calmant@isandlatech.com>
2012-07-11 14:17:49 +0200

    Ajout du charset UTF-8 à AggregatorUI
    Évite les bugs de charset dans l'UI HTML

--- demos/demo-july2012/demo.july2012.python/demo/aggregator_ui.py
+++ demos/demo-july2012/demo.july2012.python/demo/aggregator_ui.py
@@ -102,6 +102,7 @@ class AggregatorServlet(object):
         """
         output = """<html>
 <head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta http-equiv="refresh" content="2">
 <title>Sensor Aggregator - {name}</title>
 </head>
