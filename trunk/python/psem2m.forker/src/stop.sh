e9e3cdf2d9ec283c77b52b02a8152194f10f4a2b
Thomas Calmant <thomas.calmant@isandlatech.com>
2012-08-14 13:07:19 +0200

    stop.sh devient plus radical
    - kill des processus python et java, même si controller.py stop a échoué

--- trunk/python/psem2m.forker/src/stop.sh
+++ trunk/python/psem2m.forker/src/stop.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./controller.py force-stop && (ps | egrep 'java|python' | awk '{print $1}' | xargs kill)
+./controller.py force-stop ; (ps | egrep 'java|python' | awk '{print $1}' | xargs kill)
