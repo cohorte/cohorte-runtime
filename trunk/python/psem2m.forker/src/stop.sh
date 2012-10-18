1020266ea9e070cbd48f7f1a2136eef783dbface
ogattaz <olivier.gattaz@isandlatech.com>
2012-10-18 16:07:33 +0200

    adaptation à Darwin

--- trunk/python/psem2m.forker/src/stop.sh
+++ trunk/python/psem2m.forker/src/stop.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-./controller.py force-stop && (ps | egrep 'java|python' | awk '{print $1}' | xargs kill)
+
+echo "Current system : $(uname)";
+
+if [ $(uname) == "Darwin" ]
+then 
+	./controller.py force-stop && (ps -ef | egrep '/bin/java|/bin/python' | egrep 'psem2m' | awk '{print $1}' | xargs kill);
+else
+	./controller.py force-stop && (ps | egrep 'java|python' | awk '{print $1}' | xargs kill);
+fi
