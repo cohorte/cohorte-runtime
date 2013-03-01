7c7cb6446e2131f5b228d7605af9249f92026e7f
Thomas Calmant <thomas.calmant@isandlatech.com>
2013-03-01 15:27:13 +0100

    Ajout de la configuration pour démarrer des isolats Pelix
    Permet de démarrer un moniteur Pelix

--- platforms/psem2m.home/conf/boot-pelix.js
+++ platforms/psem2m.home/conf/boot-pelix.js
@@ -11,15 +11,22 @@
 	 * Isolate bundles
 	 */
 	"bundles" : [ {
+		"name" : "cohorte.boot.loaders.pelix_inner"
+	}, {
 		"name" : "cohorte.composer.agent"
 	}, {
 		"name" : "cohorte.monitor.agent"
-	}, ],
+	} ],
 
 	/**
 	 * Components
 	 */
 	"composition" : [
+	/* Loader instantiation */
+	{
+		"factory" : "cohorte-loader-pelix-factory",
+		"name" : "cohorte-loader-pelix"
+	},
 	/* Isolate agent */
 	{
 		"factory" : "cohorte-isolate-agent-factory",
---
+++ platforms/psem2m.home/conf/python-monitor.js
@@ -0,0 +1,85 @@
+/**
+ * Start configuration for the monitor (Python version)
+ */
+{
+	/*
+	 * Monitor bundles
+	 */
+	"bundles" : [
+	/* Monitor core */
+	{
+		"name" : "cohorte.monitor.core"
+	}, {
+		"name" : "cohorte.monitor.status"
+	}, {
+		"name" : "cohorte.forker.aggregator"
+	},
+
+	/* Composer */
+	{
+		"name" : "cohorte.composer.loader"
+	}, {
+		"name" : "cohorte.composer.parser"
+	}, {
+		"name" : "cohorte.composer.core.executor"
+	}, {
+		"name" : "cohorte.composer.core.queue"
+	}, {
+		"name" : "cohorte.composer.core.rating"
+	}, {
+		"name" : "cohorte.composer.core.rating"
+	}, {
+		"name" : "cohorte.composer.core.ruleengine"
+	}, {
+		"name" : "cohorte.composer.core.status"
+	} ],
+
+	/*
+	 * Components
+	 */
+	"composition" : [
+	/* Configuration of monitor components */
+	{
+		"factory" : "cohorte-monitor-core-factory",
+		"name" : "cohorte-monitor-core"
+	}, {
+		"factory" : "cohorte-monitor-status-factory",
+		"name" : "cohorte-monitor-status"
+	}, {
+		"factory" : "cohorte-forker-aggregator-factory",
+		"name" : "cohorte-forker-aggregator"
+	},
+
+	/* Composition loading */
+	{
+		"factory" : "cohorte-composer-default-checker-factory",
+		"name" : "cohorte-composer-default-checker"
+	}, {
+		"factory" : "cohorte-composer-loader-factory",
+		"name" : "cohorte-composer-loader"
+	}, {
+		"factory" : "cohorte-composer-parser-factory",
+		"name" : "cohorte-composer-parser"
+	},
+
+	/* Composer core engine */
+	{
+		"factory" : "cohorte-composer-core-executor-factory",
+		"name" : "cohorte-composer-core-executor"
+	}, {
+		"factory" : "cohorte-composer-core-queue-factory",
+		"name" : "cohorte-composer-core-queue"
+	}, {
+		"factory" : "cohorte-composer-rating-factory",
+		"name" : "cohorte-composer-rating"
+	}, {
+		"factory" : "cohorte-composer-compatibility-factory",
+		"name" : "cohorte-composer-compatibility"
+	}, {
+		"factory" : "cohorte-composer-core-rules-factory",
+		"name" : "cohorte-composer-core-rules"
+	}, {
+		"factory" : "cohorte-composer-core-status-factory",
+		"name" : "cohorte-composer-core-status"
+	} ]
+}
