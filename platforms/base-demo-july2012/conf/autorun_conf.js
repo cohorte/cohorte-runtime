a1beef284a0a7f052942ed650bc3d1c66c2dbbbc
Thomas Calmant <thomas.calmant@isandlatech.com>
2013-03-01 15:50:51 +0100

    Conversion de la composition Demo-July-2012
    Utilisation du nouveau format de fichier

--- platforms/base-demo-july2012/conf/autorun_conf.js
+++ platforms/base-demo-july2012/conf/autorun_conf.js
@@ -1,43 +1,45 @@
 {
-    "name":"demo.july2012",
-    "components":[
-        {
-            "name":"temper-1",
-            "type":"demo-temperature-fake-factory",
-            "isolate":"demo.temper",
-            "properties":{
-                "temper.value.min":-5,
-                "temper.value.max":45
-            }
-        },
-        {
-            "name":"temper-2",
-            "type":"demo-temperature-fake-factory",
-            "isolate":"demo.temper-2"
-        },
-        {
-            "name":"temper-3",
-            "type":"java-fake-temp-factory",
-            "isolate":"demo.temper-java"
-        },
-        {
-            "name":"aggregator",
-            "type":"demo-sensor-aggregator-factory",
-            "isolate":"demo.stratus.aggregator",
-            "properties":{
-                "poll.delta":1
-            }
-        },
-        {
-            "name":"aggregator-UI",
-            "type":"demo-sensor-aggregator-ui-factory",
-            "isolate":"demo.stratus.aggregator",
-            "properties":{
-                "servlet.path":"/sensors"
-            },
-            "wires":{
-                "_aggregator":"aggregator"
-            }
-        }
-    ]
+	"name": "demo-july2012-cohorte",
+	"root" : {
+		"name" : "demo-july2012",
+		"components" : [ {
+			"name" : "temper-1",
+			"type" : "demo-temperature-fake-factory",
+			"language": "python",
+			"isolate" : "demo.temper",
+			"properties" : {
+				"temper.value.min" : -5,
+				"temper.value.max" : 45
+			}
+		}, {
+			"name" : "temper-2",
+			"type" : "demo-temperature-fake-factory",
+			"language": "python",
+			"isolate" : "demo.temper-2"
+		}, {
+			"name" : "temper-3",
+			"type" : "java-fake-temp-factory",
+			"language": "java",
+			"isolate" : "demo.temper-java"
+		}, {
+			"name" : "aggregator",
+			"type" : "demo-sensor-aggregator-factory",
+			"language": "java",
+			"isolate" : "demo.stratus.aggregator",
+			"properties" : {
+				"poll.delta" : 1
+			}
+		}, {
+			"name" : "aggregator-UI",
+			"type" : "demo-sensor-aggregator-ui-factory",
+			"language": "java",
+			"isolate" : "demo.stratus.aggregator",
+			"properties" : {
+				"servlet.path" : "/sensors"
+			},
+			"wires" : {
+				"_aggregator" : "aggregator"
+			}
+		} ]
+	}
 }
