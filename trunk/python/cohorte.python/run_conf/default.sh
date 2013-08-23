b6f36cf30e4ddc89b565e7dc797dc2a2bbdb22a4
Thomas Calmant <thomas.calmant@isandlatech.com>
2013-08-23 18:16:44 +0200

    Utilisation de la nouvelle démo comme base par défaut

--- trunk/python/cohorte.python/run_conf/default.sh
+++ trunk/python/cohorte.python/run_conf/default.sh
@@ -4,4 +4,4 @@
 
 # COHORTE directories
 export COHORTE_HOME=${COHORTE_HOME:="$COHORTE_ROOT/platforms/psem2m.home"}
-export COHORTE_BASE=${COHORTE_BASE:="$COHORTE_ROOT/platforms/base-demo-july2012"}
+export COHORTE_BASE=${COHORTE_BASE:="$COHORTE_ROOT/platforms/base-demo-august2013"}
