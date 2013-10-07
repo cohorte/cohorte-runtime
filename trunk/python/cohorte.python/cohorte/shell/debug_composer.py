a8206983aa911fd8a1b8dbaae8d2edf2c44b2008
Thomas Calmant <thomas.calmant@gmail.com>
2013-10-07 16:57:42 +0200

    Added dump and inst debug commands
    - Required services are now optional, to let the commands be available
    even without all composer components validated
    - dump: dumps the content of the top status
    - inst: tries to instantiate the given file

--- trunk/python/cohorte.python/cohorte/shell/debug_composer.py
+++ trunk/python/cohorte.python/cohorte/shell/debug_composer.py
@@ -54,11 +54,16 @@ import json
 
 @ComponentFactory()
 @Requires('_reader', cohorte.SERVICE_FILE_READER)
-@Requires('_parser', cohorte.composer.SERVICE_PARSER, optional=True)
-@Requires('_distributor', cohorte.composer.SERVICE_DISTRIBUTOR_NODE, optional=True)
+@Requires('_parser', cohorte.composer.SERVICE_PARSER,
+          optional=True)
+@Requires('_distributor', cohorte.composer.SERVICE_DISTRIBUTOR_NODE,
+          optional=True)
+@Requires('_composer', cohorte.composer.SERVICE_COMPOSER_TOP,
+          optional=True)
+@Requires('_status', cohorte.composer.SERVICE_STATUS_TOP)
 @Requires('_utils', SHELL_UTILS_SERVICE_SPEC)
 @Provides(SHELL_COMMAND_SPEC)
-@Instantiate('cohorte-composer-parser-shell')
+@Instantiate('cohorte-composer-shell-debug')
 class ParserCommands(object):
     """
     Signals shell commands
@@ -68,30 +73,41 @@ class ParserCommands(object):
         Sets up members
         """
         # Injected services
-        self._parser = None
+        self._reader = None
         self._utils = None
+        self._parser = None
+        self._composer = None
+        self._distributor = None
+        self._status = None
 
         self.logger = logging.getLogger('composer-shell')
+
 
     def get_namespace(self):
         """
         Retrieves the name space of this command handler
         """
-        return "parser"
+        return "composer"
 
 
     def get_methods(self):
         """
         Retrieves the list of tuples (command, method) for this command handler
         """
-        return [("load", self.load_composition),
-                ("read", self.read_file)]
+        return [("distribute", self.distribute_composition),
+                ("read", self.read_file),
+                ('dump', self.dump_status),
+                ('inst', self.instantiate_composition)]
 
 
-    def read_file(self, io_handler, filename, base="conf"):
+    def read_file(self, io_handler, filename="autorun_conf.js", base="conf"):
         """
         Reads a file
         """
+        if self._reader is None:
+            io_handler.write_line("No file reader.")
+            return
+
         # Read the file content (dictionary)
         data = self._reader.load_file(filename, base)
 
@@ -101,15 +117,63 @@ class ParserCommands(object):
                                                 separators=(',', ': ')))
 
 
-    def load_composition(self, io_handler, filename, base="conf"):
+    def distribute_composition(self, io_handler,
+                               filename="autorun_conf.js", base="conf"):
         """
-        Parses a composition
+        Parses a composition and computes its node distribution
         """
+        if self._parser is None:
+            io_handler.write_line("No parser found.")
+            return
+
         # Load the composition
         composition = self._parser.load(filename, base)
+
+        if self._distributor is None:
+            io_handler.write_line("No distributor found.")
+            io_handler.write_line("Composition: {0}", str(composition))
+            return
 
         # Distribute it
         distribution = self._distributor.distribute(composition)
 
         # Pretty print
         io_handler.write_line('{0}', pformat(distribution, indent=2))
+
+
+    def instantiate_composition(self, io_handler,
+                                filename="autorun_conf.js", base="conf"):
+        """
+        Instantiates the given composition
+        """
+        if self._parser is None:
+            io_handler.write_line("No parser found.")
+            return
+
+        # Load the composition
+        composition = self._parser.load(filename, base)
+
+        # Tell the top composer to work
+        if self._composer is None:
+            io_handler.write_line("No composer found.")
+            return
+
+        uid = self._composer.start(composition)
+        io_handler.write_line("Started composition: {0} -> {1}",
+                              composition.name, uid)
+
+
+    def dump_status(self, io_handler, node=None):
+        """
+        Dumps the content of status
+        """
+        if self._status is None:
+            io_handler.write_line("No status found.")
+            return
+
+        if not node:
+            io_handler.write_line("{0}", self._status.dump())
+
+        else:
+            io_handler.write_line("{0}",
+                          pformat(self._status.get_components_for_node(node)))
