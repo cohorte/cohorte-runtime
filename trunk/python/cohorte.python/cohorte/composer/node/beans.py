e8165a7ff68ec3e45ff95b8b52c341a9e11d2c4a
Thomas Calmant <thomas.calmant@gmail.com>
2013-10-10 17:16:51 +0200

    Isolate bean can accept/reject renaming
    Allows a non-named isolate to be renamed by an elector

--- trunk/python/cohorte.python/cohorte/composer/node/beans.py
+++ trunk/python/cohorte.python/cohorte/composer/node/beans.py
@@ -35,18 +35,33 @@ __docformat__ = "restructuredtext en"
 
 # ------------------------------------------------------------------------------
 
+import itertools
+
+# ------------------------------------------------------------------------------
+
 class Isolate(object):
     """
     Represents an isolate to be instantiated
     """
+    # The isolate counter
+    __counter = itertools.count(1)
+
     def __init__(self, name=None):
         """
         Sets up members
 
         :param name: The name of the isolate
         """
-        self.name = name
+        # The configured isolate name
+        self.__name = name
+
+        # Proposed name, if the current vote passes
+        self.__proposed_name = None
+
+        # Language of components hosted by this isolate
         self.language = None
+
+        # Components hosted by this isolate
         self.__components = set()
 
 
@@ -57,7 +72,79 @@ class Isolate(object):
         if not self.language:
             return "NeutralIsolate"
 
-        return "Isolate({0}, {1})".format(self.name, self.language)
+        return "Isolate({0}, {1})".format(self.__name, self.language)
+
+
+    def accepted_rename(self):
+        """
+        Possible name accepted
+
+        :raise ValueError: A name was already given
+        """
+        if self.__name:
+            raise ValueError("Isolate already have a name: {0}" \
+                             .format(self.__name))
+
+        self.__name = self.__proposed_name
+        self.__proposed_name = None
+
+
+    def propose_rename(self, new_name):
+        """
+        Proposes the renaming of the isolate
+
+        :raise ValueError: A name was already given to this isolate
+        :return: True if the proposal is acceptable
+        """
+        if self.__name:
+            raise ValueError("Isolate already have a name: {0}" \
+                             .format(self.__name))
+
+        if self.__proposed_name:
+            return False
+
+        self.__proposed_name = new_name
+        return True
+
+
+    def rejected_rename(self):
+        """
+        Possible name rejected
+        """
+        self.__proposed_name = None
+
+
+    def generate_name(self, node):
+        """
+        Generates a name for this isolate (to be called) after votes.
+        Does nothing if a name was already assigned to the isolate
+
+        :param node: The node name
+        :return: The (generated) isolate name
+        """
+        if not self.__name:
+            # Need to generate a name
+            self.__name = '{node}-{language}-auto{count:02d}' \
+                          .format(node=node, language=self.language,
+                                  count=next(Isolate.__counter))
+
+        return self.__name
+
+
+    @property
+    def name(self):
+        """
+        Returns the name of the isolate
+        """
+        return self.__name
+
+
+    @property
+    def proposed_name(self):
+        """
+        Returns the currently proposed name, or None
+        """
+        return self.__proposed_name
 
 
     @property
