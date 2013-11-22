a4d5abe2c7584b842fdd02b0213e8d2f035e6196
Thomas Calmant <thomas.calmant@gmail.com>
2013-11-22 11:48:59 +0100

    Prettier remote Shell utility script
    Supports request of monitor port to the forker
    Supports a forced port argument
    Validates that the found port is an integer

--- trunk/python/cohorte.python/rshell.sh
+++ trunk/python/cohorte.python/rshell.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-port=$(echo shells | nc localhost 8001 | grep cohorte.internals.monitor | cut -d "|" -f 5)
+# Get the port
+if [ -z "$1" ]
+then
+	port=$(echo shells | nc localhost 8001 | grep cohorte.internals.monitor | cut -d "|" -f 5 | tr -d ' ')
+else
+	port="$1"
+fi
+
+# Validate it
+int_regex='^[0-9]+$'
+if ! [[ $port =~ $int_regex ]]
+then
+	echo "Not a number: $port"
+	exit 1
+fi
+
+echo "Connecting to localhost:$port..."
 rlwrap nc localhost $port
 
