887ece269afdafbf699779be6b47fb25385ddece
Thomas Calmant <thomas.calmant@gmail.com>
2013-12-03 16:01:57 +0100

    Changed rshell.sh default port to 8001

--- trunk/python/cohorte.python/rshell.sh
+++ trunk/python/cohorte.python/rshell.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Default Monitor shell port
-DEFAULT_PORT=8011
+DEFAULT_PORT=8001
 
 # Get the port
 if [ -z "$1" ]
 then
-	port=$(echo shells | nc localhost 8001 | grep cohorte.internals.monitor | cut -d "|" -f 5 | tr -d ' ')
+    port=$(echo shells | nc localhost 8001 | grep cohorte.internals.monitor | cut -d "|" -f 5 | tr -d ' ')
 else
-	port="$1"
+    port="$1"
 fi
 
 # Validate it
 int_regex='^[0-9]+$'
 if ! [[ $port =~ $int_regex ]]
 then
-	echo "Not a number: $port"
-	port=$DEFAULT_PORT
+    echo "Not a number: $port"
+    port=$DEFAULT_PORT
 fi
 
 echo "Connecting to localhost:$port..."
