e3ded5bcf39208394d9ca29a1632499f1eb1d74a
Thomas Calmant <thomas.calmant@gmail.com>
2013-12-10 18:42:09 +0100

    Added a sample isolate autorun configuration

---
+++ platforms/base-demo-august2013/conf/autorun_isolates.js
@@ -0,0 +1,9 @@
+[ {
+	"node" : "central",
+	"name" : "vlc",
+	"kind" : "exe",
+	"executable" : "/usr/bin/openoffice.org",
+	// "arguments" : [ "/home/tcalmant/Vidéos/forkit.mpeg" ]
+	"arguments" : [ "/home/tcalmant/Documents/forker.ods" ],
+	"io_watch" : false
+} ]
