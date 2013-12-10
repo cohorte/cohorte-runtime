bb23319f3568cf1a36168b3777cb001b17d886c0
Thomas Calmant <thomas.calmant@gmail.com>
2013-12-10 18:39:55 +0100

    Less blocking watcher
    - I/O watcher is optional
    - I/O unwatch thread join() has now a timeout (of .2s)
    - Using a ThreadPool of 1 thread to notify listeners

--- trunk/python/cohorte.python/cohorte/forker/watchers.py
+++ trunk/python/cohorte.python/cohorte/forker/watchers.py
@@ -19,11 +19,12 @@ __version__ = "1.0.0"
 
 # Cohorte
 import cohorte.forker
-import cohorte.utils as utils
+import cohorte.utils
 
 # Pelix framework
 from pelix.ipopo.decorators import ComponentFactory, Validate, Invalidate, \
     Provides, Requires, Instantiate
+import pelix.threadpool
 
 # Python standard library
 import logging
@@ -69,6 +70,9 @@ class IsolateWatcher(object):
         # Pause between loops (in seconds)
         self._pause = .5
 
+        # Notification thread
+        self._pool = pelix.threadpool.ThreadPool(1, logname="IOWatcher-Notify")
+
         # Some locking
         self.__lock = threading.Lock()
 
@@ -82,7 +86,10 @@ class IsolateWatcher(object):
         self._stop_event.clear()
 
         # Get utility methods
-        self._utils = utils.get_os_utils()
+        self._utils = cohorte.utils.get_os_utils()
+
+        # Start the notification thread
+        self._pool.start()
 
         # Start the waiting thread
         self._wait_thread = threading.Thread(target=self.__wait_thread,
@@ -106,10 +113,26 @@ class IsolateWatcher(object):
         for thread, _ in list(self._io_threads.values()):
             thread.join()
 
+        # Stop the notification thread
+        self._pool.stop()
+
         # Clean up
         self._isolates.clear()
         self._wait_thread = None
         self._io_threads.clear()
+
+
+    def _notify_listeners(self, listeners, uid):
+        """
+        Notifies listeners of the loss of an isolate
+
+        :param listeners: Listeners to notify
+        :param uid: UID of the lost isolate
+        """
+        _logger.debug("Notifying listeners...")
+        for listener in listeners:
+            _logger.debug('... notifying %s', listener)
+            listener.handle_lost_isolate(uid)
 
 
     def __wait_thread(self):
@@ -126,7 +149,7 @@ class IsolateWatcher(object):
             for uid, process in isolates:
                 try:
                     # Check PID presence
-                    self._utils.wait_pid(process.pid, 0)
+                    self._utils.wait_pid(process.pid, .1)
 
                     # Being here means that the process ended,
                     # ... clean up
@@ -134,10 +157,11 @@ class IsolateWatcher(object):
 
                     # ... notify listeners
                     if self._listeners:
-                        for listener in self._listeners:
-                            listener.handle_lost_isolate(uid)
+                        listeners = self._listeners[:]
+                        self._pool.enqueue(self._notify_listeners,
+                                           listeners, uid)
 
-                except utils.TimeoutExpired:
+                except cohorte.utils.TimeoutExpired:
                     # Time out expired : process is still there,
                     # continue the loop
                     pass
@@ -156,8 +180,7 @@ class IsolateWatcher(object):
 
         while not event.is_set():
             line = process.stdout.readline()
-            if line == b'':
-                # Sentinel: no more input from the process
+            if not line:
                 break
 
             try:
@@ -171,12 +194,13 @@ class IsolateWatcher(object):
             logger.debug(line)
 
 
-    def watch(self, uid, process):
+    def watch(self, uid, process, watch_io=True):
         """
         Watch for the given isolate
 
         :param uid: Isolate UID
         :param process: Isolate process (from subprocess module)
+        :param watch_io: If True, start an I/O printer thread
         :raise KeyError: Already known UID
         """
         with self.__lock:
@@ -186,16 +210,17 @@ class IsolateWatcher(object):
             # Store the process
             self._isolates[uid] = process
 
-            # Start the I/O thread
-            event = threading.Event()
-            thread = threading.Thread(target=self.__io_thread,
-                                      name="IOWatcher-{0}".format(uid),
-                                      args=[uid, process, event])
-            thread.daemon = True
-            thread.start()
+            if watch_io:
+                # Start the I/O thread
+                event = threading.Event()
+                thread = threading.Thread(target=self.__io_thread,
+                                          args=(uid, process, event),
+                                          name="IOWatcher-{0}".format(uid))
+                thread.daemon = True
+                thread.start()
 
-            # Store it
-            self._io_threads[uid] = (thread, event)
+                # Store it
+                self._io_threads[uid] = (thread, event)
 
 
     def unwatch(self, uid):
@@ -203,14 +228,20 @@ class IsolateWatcher(object):
         Stop watching an isolate
 
         :param uid: An isolate UID
+        :raise KeyError: Unknown UID
         """
         with self.__lock:
             # Remove from waited isolates
-            del self._isolates[uid]
+            self._isolates.pop(uid)
 
             # Remove from the list of I/O threads
-            thread, event = self._io_threads.pop(uid)
+            try:
+                thread, event = self._io_threads.pop(uid)
+
+            except KeyError:
+                # I/O watcher wasn't requested
+                return
 
         # Stop the I/O thread (out of lock)
         event.set()
-        thread.join()
+        thread.join(.2)
