a9fb382551e25b849eb2c826287b105111b01faa
Thomas Calmant <thomas.calmant@isandlatech.com>
2014-09-15 10:49:29 +0200

    Use Herald-RPC in Java

--- platforms/psem2m.home/conf/java-common-remote.js
+++ platforms/psem2m.home/conf/java-common-remote.js
@@ -11,7 +11,10 @@
 		"name" : "org.cohorte.remote.utilities"
 	}, {
 		"name" : "org.cohorte.remote.core"
-	} /* {
+	}, {
+		"name" : "org.cohorte.herald.rpc"
+	}
+	/* {
 		"name" : "org.cohorte.remote.dispatcher.servlet"
 	}, {
 		"name" : "org.cohorte.remote.discovery.signals"
