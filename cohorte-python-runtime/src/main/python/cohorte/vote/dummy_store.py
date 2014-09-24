97f252106dcee32f3e5273bac5c0a19e25b404ae
Thomas Calmant <thomas.calmant@isandlatech.com>
2014-09-24 15:30:20 +0200

    Added missing dummy vote storage bundle

---
+++ cohorte-python-runtime/src/main/python/cohorte/vote/dummy_store.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+# -- Content-Encoding: UTF-8 --
+"""
+:author: Thomas Calmant
+:copyright: Copyright 2014, isandlaTech
+:license: Apache License 2.0
+:version: 0.0.1
+:status: Alpha
+
+..
+
+    Copyright 2014 isandlaTech
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+# Module version
+__version_info__ = (0, 0, 1)
+__version__ = ".".join(str(x) for x in __version_info__)
+
+# Documentation strings format
+__docformat__ = "restructuredtext en"
+
+# ------------------------------------------------------------------------------
+
+# Voting system
+import cohorte.vote
+
+# iPOPO Decorators
+from pelix.ipopo.decorators import ComponentFactory, Provides, Requires, \
+    Instantiate, Property, Invalidate
+import pelix.http
+
+# Standard library
+import logging
+
+# ------------------------------------------------------------------------------
+
+_logger = logging.getLogger(__name__)
+
+# ------------------------------------------------------------------------------
+
+@ComponentFactory()
+@Provides(cohorte.vote.SERVICE_VOTE_STORE)
+@Instantiate('dummy-vote-store')
+class DummyVoteStore(object):
+    """
+    Mimics a vote store
+    """
+    def store_vote(self, vote):
+        """
+        We said dummy
+        """
+        pass
