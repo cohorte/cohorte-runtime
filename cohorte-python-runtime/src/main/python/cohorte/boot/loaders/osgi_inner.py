fae9b8456930396f744ccf8115db5594f6c59793
Thomas Calmant <thomas.calmant@isandlatech.com>
2014-09-26 12:04:25 +0200

    Propagate the application ID to the Java OSGi framework

--- cohorte-python-runtime/src/main/python/cohorte/boot/loaders/osgi_inner.py
+++ cohorte-python-runtime/src/main/python/cohorte/boot/loaders/osgi_inner.py
@@ -354,7 +354,8 @@ class JavaOsgiLoader(object):
                     cohorte.PROP_NODE_UID, cohorte.PROP_NODE_NAME,
                     cohorte.PROP_DUMPER_PORT,
                     herald.FWPROP_PEER_UID, herald.FWPROP_PEER_NAME,
-                    herald.FWPROP_NODE_UID, herald.FWPROP_NODE_NAME):
+                    herald.FWPROP_NODE_UID, herald.FWPROP_NODE_NAME,
+                    herald.FWPROP_APPLICATION_ID):
             value = self._context.get_property(key)
             if value is not None:
                 # Avoid empty values
