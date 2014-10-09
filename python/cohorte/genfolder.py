ff2f429ecf2ac18627f133315d02c6832de5ad6a
Thomas Calmant <thomas.calmant@isandlatech.com>
2014-10-09 16:18:48 +0200

    First implementation of cohorte.genfolder
    A utility script to grab and extract template ZIP files from isandlaTech web servers

---
+++ python/cohorte/genfolder.py
@@ -0,0 +1,209 @@
+#!/usr/bin/python
+# -- Content-Encoding: UTF-8 --
+"""
+Cohorte quick start module, which grabs the home and base templates from
+isandlaTech servers
+
+:author: Thomas Calmant
+:copyright: Copyright 2014, isandlaTech
+:license: Apache License 2.0
+:version: 0.0.1
+:status: Alpha
+
+..
+
+    Copyright 2014 isandlaTech
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+# Module version
+__version_info__ = (0, 0, 1)
+__version__ = ".".join(str(x) for x in __version_info__)
+
+# Documentation strings format
+__docformat__ = "restructuredtext en"
+
+import argparse
+import json
+import os
+import sys
+import tempfile
+import zipfile
+
+from pelix.utilities import to_str
+
+
+try:
+    import urllib2
+except ImportError:
+    import urllib.request as urllib2
+
+
+class CohorteMaker(object):
+    """
+    Utility class to construct home and bases folders
+    """
+    def __init__(self, index_url):
+        """
+        Sets up members
+
+        :param index_url: URL to the JSON index file
+        """
+        self.index_url = index_url
+        # Name -> ZIP URL
+        self._index = {}
+        # Name -> TemporaryFile
+        self._templates = {}
+
+    def _get_file_content(self, url):
+        """
+        Returns the content of the given file
+
+        :param url: URL to the file
+        :return: The content of the file
+        :raise IOError: Error loading the file
+        """
+        fp = urllib2.urlopen(url)
+        return fp.read()
+
+    def load_index(self, force=False):
+        """
+        Loads the templates index
+
+        :param force: Force index reload
+        :raise IOError: Can't load the index file
+        :raise ValueError: Invalid index file
+        """
+        if force or not self._index:
+            data = to_str(self._get_file_content(self.index_url))
+            index = json.loads(data)
+            for key in ('home', 'base'):
+                self._index[key] = \
+                    index['snapshots']['cohorte-minimal-{0}-distribution'
+                                       .format(key)]
+
+    def load_template(self, name, force=False):
+        """
+        Loads the template with the given name
+
+        :param name: Name of the template
+        :param force: If True, force the template to be reloaded
+        :raise IOError: Error loading the file
+        :raise KeyError: Unknown template
+        """
+        if force or name not in self._templates:
+            # Download the template
+            content = self._get_file_content(self._index[name])
+            fp = tempfile.TemporaryFile()
+            fp.write(content)
+            fp.flush()
+            self._templates[name] = fp
+
+    def extract_template(self, name, directory):
+        """
+        Extract the template to the disk
+
+        :param name: Name of the template
+        :param directory: Output directory
+        :raise KeyError: Unknown template
+        :raise IOError: Error extracting template
+        """
+        tpl_zip = zipfile.ZipFile(self._templates[name])
+        tpl_zip.extractall(directory)
+        tpl_zip.close()
+
+    def make_home_directory(self, home_dir):
+        """
+        Prepares the Cohorte Home directory
+
+        :param home_dir: The absolute path to the home directory
+        :raise IOError: Error creating the folder
+        """
+        if os.path.exists(home_dir):
+            raise IOError("Home directory {0} already exist. Abandon."
+                          .format(home_dir))
+
+        # Load the template file
+        self.load_index(False)
+        self.load_template("home", False)
+
+        self.extract_template("home", home_dir)
+        print("Home template extracted, you know have to set the COHORTE_HOME "
+              "environment variable:")
+        print("set COHORTE_HOME=\"{0}\"".format(home_dir))
+
+    def make_base_directory(self, base_dir):
+        """
+        Prepares the Cohorte Base directory
+
+        :param base_dir: The absolute path to the base directory
+        """
+        if os.path.exists(base_dir):
+            raise IOError("Base directory {0} already exist. Abandon."
+                          .format(base_dir))
+
+        # Load the template file
+        self.load_index(False)
+        self.load_template("base", False)
+
+        # Extract it
+        self.extract_template("base", base_dir)
+        print("Base template extracted, you know have to set the COHORTE_BASE "
+              "environment variable:")
+        print("set COHORTE_BASE=\"{0}\"".format(base_dir))
+
+
+def main(args=None):
+    """
+    Script entry point
+    :param args: Program arguments
+    """
+    parser = argparse.ArgumentParser("Cohorte Quick start")
+    parser.add_argument("--home", dest="home_dir", metavar="HOME_DIR",
+                        help="Directory where to store the Cohorte Home"
+                             "folder")
+    parser.add_argument("-b", "--base", dest="base_dirs", nargs="*",
+                        metavar="BASE_DIR",
+                        help="Directory where to store the Cohorte Base"
+                             "folder")
+    parser.add_argument("--url", dest="index_url", metavar="URL",
+                        default="http://repo.isandlatech.com/"
+                                "last_snapshots.json",
+                        help="Index JSON file")
+    options = parser.parse_args(args)
+    if not any((options.home_dir, options.base_dirs)):
+        parser.print_help()
+        return 1
+
+    # Prepare the maker
+    maker = CohorteMaker(options.index_url)
+    try:
+        if options.home_dir:
+            # Extract the template
+            target_dir = os.path.abspath(options.home_dir)
+            maker.make_home_directory(target_dir)
+
+        if options.base_dirs:
+            for base_dir in options.base_dirs:
+                # Create the base directory
+                target_dir = os.path.abspath(base_dir)
+                maker.make_base_directory(target_dir)
+
+    except IOError as ex:
+        print(ex)
+        return 1
+
+
+if __name__ == '__main__':
+    sys.exit(main())
