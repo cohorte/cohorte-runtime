fe433b1db79ee6665283c9ac44a23bcac800cf8a
debbabi <dbassem@gmail.com>
2015-03-09 18:02:29 +0100

    Prepare Cohorte Runtime Python 1.0.0 release

--- python/setup.py
+++ python/setup.py
@@ -6,7 +6,7 @@ iPOPO installation script
 :author: Thomas Calmant
 :copyright: Copyright 2014, isandlaTech
 :license: Apache License 2.0
-:version: 1.0.0.dev
+:version: 1.0.0
 :status: Beta
 
 ..
@@ -27,7 +27,7 @@ iPOPO installation script
 """
 
 # Module version
-__version_info__ = (1, 0, 0, "dev")
+__version_info__ = (1, 0, 0)
 __version__ = ".".join(str(x) for x in __version_info__)
 
 # Documentation strings format
@@ -101,7 +101,7 @@ setup(
     ],
     install_requires=[
         'iPOPO >= 0.5.7',
-        'Cohorte-Herald >= 0.0.2.dev'
+        'Cohorte-Herald >= 0.0.3'
     ],
     extras_require={
         'java': ['JPype1-py3']
