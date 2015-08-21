aa3ddca07bec261444d06bf4e05e97b78593f587
ogattaz <olivier@gattaz.com>
2015-08-21 17:33:22 +0200

    Adjust dependency version

--- java/tools/org.psem2m.isolates.jmx.monitor/src/org/psem2m/isolates/jmx/monitor/Activator.java
+++ java/tools/org.psem2m.isolates.jmx.monitor/src/org/psem2m/isolates/jmx/monitor/Activator.java
@@ -13,56 +13,56 @@ import org.psem2m.isolates.services.monitoring.IThreadCpuUsageMonitor;
 
 /**
  * JMX thread monitor bundle activator
- * 
+ *
  * @author Thomas Calmant
  */
 public class Activator implements BundleActivator {
 
-    /** The bundle context */
-    private static BundleContext sContext;
+	/** The bundle context */
+	private static BundleContext sContext;
 
-    /**
-     * Retrieves the current bundle context
-     * 
-     * @return The bundle context
-     */
-    public static BundleContext getContext() {
+	/**
+	 * Retrieves the current bundle context
+	 * 
+	 * @return The bundle context
+	 */
+	public static BundleContext getContext() {
 
-        return sContext;
-    }
+		return sContext;
+	}
 
-    /** The JMX Monitor service registration */
-    private ServiceRegistration<IThreadCpuUsageMonitor> pJmxMonitorRegistration;
+	/** The JMX Monitor service registration */
+	private ServiceRegistration<IThreadCpuUsageMonitor> pJmxMonitorRegistration;
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext
-     * )
-     */
-    @Override
-    public void start(final BundleContext aBundleContext) throws Exception {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext
+	 * )
+	 */
+	@Override
+	public void start(final BundleContext aBundleContext) throws Exception {
 
-        sContext = aBundleContext;
+		sContext = aBundleContext;
 
-        // Register the JMX Monitor service
-        pJmxMonitorRegistration = aBundleContext.registerService(
-                IThreadCpuUsageMonitor.class, new JmxMonitor(), null);
-    }
+		// Register the JMX Monitor service
+		pJmxMonitorRegistration = aBundleContext.registerService(
+				IThreadCpuUsageMonitor.class, new JmxMonitor(), null);
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-     */
-    @Override
-    public void stop(final BundleContext aBundleContext) throws Exception {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void stop(final BundleContext aBundleContext) throws Exception {
 
-        sContext = null;
+		sContext = null;
 
-        // Unregister the JMX monitor service
-        pJmxMonitorRegistration.unregister();
-    }
+		// Unregister the JMX monitor service
+		pJmxMonitorRegistration.unregister();
+	}
 }
--- java/tools/org.psem2m.isolates.jmx.monitor/src/org/psem2m/isolates/jmx/monitor/impl/JmxMonitor.java
+++ java/tools/org.psem2m.isolates.jmx.monitor/src/org/psem2m/isolates/jmx/monitor/impl/JmxMonitor.java
@@ -15,155 +15,156 @@ import org.psem2m.isolates.services.monitoring.IThreadCpuUsageMonitor;
 
 /**
  * Uses JMX to poll the Thread MXBean and compute
- * 
+ *
  * @author Thomas Calmant
  */
 public class JmxMonitor implements IThreadCpuUsageMonitor {
 
-    /** JMX Thread MXBean */
-    private ThreadMXBean pThreadBean;
+	/** JMX Thread MXBean */
+	private final ThreadMXBean pThreadBean;
 
-    /** The threads times */
-    private final Map<Long, ThreadTimes> pTimes = new HashMap<Long, ThreadTimes>();
+	/** The threads times */
+	private final Map<Long, ThreadTimes> pTimes = new HashMap<Long, ThreadTimes>();
 
-    /**
-     * Prepares the beans
-     */
-    public JmxMonitor() {
+	/**
+	 * Prepares the beans
+	 */
+	public JmxMonitor() {
 
-        pThreadBean = ManagementFactory.getThreadMXBean();
-    }
+		pThreadBean = ManagementFactory.getThreadMXBean();
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.psem2m.isolates.jmx.monitor.impl.ICpuThreadMonitor#getAverageCpuUsage
-     * ()
-     */
-    @Override
-    public double getAverageCpuUsage() {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.psem2m.isolates.jmx.monitor.impl.ICpuThreadMonitor#getAverageCpuUsage
+	 * ()
+	 */
+	@Override
+	public double getAverageCpuUsage() {
 
-        double averageCpuTime = 0;
-        int validThreads = 0;
+		double averageCpuTime = 0;
+		int validThreads = 0;
 
-        for (ThreadTimes threadTime : pTimes.values()) {
+		for (final ThreadTimes threadTime : pTimes.values()) {
 
-            // Sum all average CPU usage
-            double threadAverageCpuTime = threadTime.getAverageCpuTimeUsage();
+			// Sum all average CPU usage
+			final double threadAverageCpuTime = threadTime
+					.getAverageCpuTimeUsage();
 
-            if (threadAverageCpuTime > 0) {
-                // Only use valid thread times
-                validThreads++;
-                averageCpuTime += threadTime.getAverageCpuTimeUsage();
-            }
-        }
+			if (threadAverageCpuTime > 0) {
+				// Only use valid thread times
+				validThreads++;
+				averageCpuTime += threadTime.getAverageCpuTimeUsage();
+			}
+		}
 
-        if (validThreads == 0) {
-            // No valid data...
-            return 0;
-        }
+		if (validThreads == 0) {
+			// No valid data...
+			return 0;
+		}
 
-        return averageCpuTime / validThreads;
-    }
+		return averageCpuTime / validThreads;
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.psem2m.isolates.jmx.monitor.impl.ICpuThreadMonitor#getKnownThreadsIds
-     * ()
-     */
-    @Override
-    public long[] getKnownThreadsIds() {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.psem2m.isolates.jmx.monitor.impl.ICpuThreadMonitor#getKnownThreadsIds
+	 * ()
+	 */
+	@Override
+	public long[] getKnownThreadsIds() {
 
-        long[] result = new long[pTimes.size()];
-        int i = 0;
+		final long[] result = new long[pTimes.size()];
+		int i = 0;
 
-        for (Long threadId : pTimes.keySet()) {
-            // Convert all Long objects to primitive
-            result[i] = threadId;
-            i++;
-        }
+		for (final Long threadId : pTimes.keySet()) {
+			// Convert all Long objects to primitive
+			result[i] = threadId;
+			i++;
+		}
 
-        // Sort the array
-        Arrays.sort(result);
-        return result;
-    }
+		// Sort the array
+		Arrays.sort(result);
+		return result;
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.psem2m.isolates.jmx.monitor.impl.ICpuThreadMonitor#
-     * getThreadAverageCpuUsage(long)
-     */
-    @Override
-    public double getThreadAverageCpuUsage(final long aThreadId) {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.psem2m.isolates.jmx.monitor.impl.ICpuThreadMonitor#
+	 * getThreadAverageCpuUsage(long)
+	 */
+	@Override
+	public double getThreadAverageCpuUsage(final long aThreadId) {
 
-        final ThreadTimes threadTimes = pTimes.get(aThreadId);
-        if (threadTimes == null) {
-            return -1;
-        }
+		final ThreadTimes threadTimes = pTimes.get(aThreadId);
+		if (threadTimes == null) {
+			return -1;
+		}
 
-        return threadTimes.getTotalAverageCpuTimeUsage();
-    }
+		return threadTimes.getTotalAverageCpuTimeUsage();
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.psem2m.isolates.services.monitoring.IThreadCpuUsageMonitor#
-     * getThreadCpuUsage(long)
-     */
-    @Override
-    public double getThreadCpuUsage(final long aThreadId) {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.psem2m.isolates.services.monitoring.IThreadCpuUsageMonitor#
+	 * getThreadCpuUsage(long)
+	 */
+	@Override
+	public double getThreadCpuUsage(final long aThreadId) {
 
-        final ThreadTimes threadTimes = pTimes.get(aThreadId);
-        if (threadTimes == null) {
-            return -1;
-        }
+		final ThreadTimes threadTimes = pTimes.get(aThreadId);
+		if (threadTimes == null) {
+			return -1;
+		}
 
-        return threadTimes.getAverageCpuTimeUsage();
-    }
+		return threadTimes.getAverageCpuTimeUsage();
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.psem2m.isolates.jmx.monitor.impl.ICpuThreadMonitor#update()
-     */
-    @Override
-    public void update() {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.psem2m.isolates.jmx.monitor.impl.ICpuThreadMonitor#update()
+	 */
+	@Override
+	public void update() {
 
-        if (!pThreadBean.isThreadCpuTimeSupported()) {
-            // Thread CPU Time not supported, do nothing
-            return;
-        }
+		if (!pThreadBean.isThreadCpuTimeSupported()) {
+			// Thread CPU Time not supported, do nothing
+			return;
+		}
 
-        // Work on all live thread
-        final long[] threadsIds = pThreadBean.getAllThreadIds();
-        for (long threadId : threadsIds) {
+		// Work on all live thread
+		final long[] threadsIds = pThreadBean.getAllThreadIds();
+		for (final long threadId : threadsIds) {
 
-            // Get thread times
-            final long cpuTime = pThreadBean.getThreadCpuTime(threadId);
-            final long userTime = pThreadBean.getThreadCpuTime(threadId);
+			// Get thread times
+			final long cpuTime = pThreadBean.getThreadCpuTime(threadId);
+			final long userTime = pThreadBean.getThreadCpuTime(threadId);
 
-            if (cpuTime == -1 || userTime == -1) {
-                // Dead thread
-                pTimes.remove(threadId);
-                continue;
-            }
+			if (cpuTime == -1 || userTime == -1) {
+				// Dead thread
+				pTimes.remove(threadId);
+				continue;
+			}
 
-            // Get the stored thread time
-            final ThreadTimes threadTimes = pTimes.get(threadId);
+			// Get the stored thread time
+			final ThreadTimes threadTimes = pTimes.get(threadId);
 
-            if (threadTimes == null) {
-                // New thread
-                pTimes.put(threadId, new ThreadTimes(threadId, cpuTime,
-                        userTime));
+			if (threadTimes == null) {
+				// New thread
+				pTimes.put(threadId, new ThreadTimes(threadId, cpuTime,
+						userTime));
 
-            } else {
-                // Update the thread times
-                threadTimes.updateTimes(cpuTime, userTime);
-            }
-        }
-    }
+			} else {
+				// Update the thread times
+				threadTimes.updateTimes(cpuTime, userTime);
+			}
+		}
+	}
 }
